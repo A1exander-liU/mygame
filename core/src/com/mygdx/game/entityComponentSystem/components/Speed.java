f47565e800b704f366e9cbfc58635c4518aac952
AlexanderLiu <alexanderliu2016@gmail.com>
2022-06-30 15:32:41 -0700

    increase enemy speed values

--- core/src/com/mygdx/game/entityComponentSystem/components/Speed.java
+++ core/src/com/mygdx/game/entityComponentSystem/components/Speed.java
@@ -3,6 +3,6 @@ package com.mygdx.game.entityComponentSystem.components;
 import com.badlogic.ashley.core.Component;
 
 public class Speed implements Component {
-    public float xSpeed = 5;
-    public float ySpeed = 5;
+    public float xSpeed = 15;
+    public float ySpeed = 15;
 }
