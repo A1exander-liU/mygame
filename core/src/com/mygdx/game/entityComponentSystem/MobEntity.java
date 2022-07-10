135e6e1da1b3f157e273e39d110a0c5c7df64698
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-10 15:37:52 -0700

    make MobEntity class with basic constructor

---
+++ core/src/com/mygdx/game/entityComponentSystem/MobEntity.java
@@ -0,0 +1,26 @@
+package com.mygdx.game.entityComponentSystem;
+
+import com.badlogic.ashley.core.Entity;
+import com.mygdx.game.GameMapProperties;
+import com.mygdx.game.MyGame;
+import com.mygdx.game.entityComponentSystem.components.Enemy;
+import com.mygdx.game.entityComponentSystem.components.EntitySprite;
+import com.mygdx.game.entityComponentSystem.components.Health;
+import com.mygdx.game.entityComponentSystem.components.ID;
+import com.mygdx.game.entityComponentSystem.components.Name;
+import com.mygdx.game.entityComponentSystem.components.Position;
+import com.mygdx.game.entityComponentSystem.components.Size;
+import com.mygdx.game.entityComponentSystem.components.Speed;
+
+public class MobEntity extends Entity {
+    ComponentGrabber cg;
+    MyGame root;
+    GameMapProperties gameMapProperties;
+
+
+    public MobEntity(ComponentGrabber cg, MyGame root, GameMapProperties gameMapProperties) {
+        this.cg = cg;
+        this.root = root;
+        this.gameMapProperties = gameMapProperties;
+    }
+}
