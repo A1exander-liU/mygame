38cefe9d53d8c7b71b51ffc155c26b21d06b6ab3
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-15 16:31:38 -0700

    remove random x and y

--- core/src/com/mygdx/game/entityComponentSystem/components/Position.java
+++ core/src/com/mygdx/game/entityComponentSystem/components/Position.java
@@ -9,13 +9,6 @@ import java.util.Random;
 
 // component are only "data bags" (no logic/methods/actions, just data)
 public class Position implements Component {
-    Random random = new Random();
-    TiledMap tiledMap = new TmxMapLoader().load("untitled.tmx");
-    GameMapProperties gameMapProperties = new GameMapProperties(tiledMap);
-    float randomX = random.nextInt(gameMapProperties.mapWidth - 100);
-    float randomY = random.nextInt(gameMapProperties.mapHeight - 100);
-    public float oldX = randomX;
-    public float oldY = randomY;
-    public float x = randomX;
-    public float y = randomY;
+    public float x;
+    public float y;
 }
