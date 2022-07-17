61061781c9a6889ce15ad3a0632c5d0875d45adf
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-17 16:16:12 -0700

    add ai component

---
+++ core/src/com/mygdx/game/entityComponentSystem/components/AI.java
@@ -0,0 +1,126 @@
+package com.mygdx.game.entityComponentSystem.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.ai.steer.Steerable;
+import com.badlogic.gdx.ai.utils.Location;
+import com.badlogic.gdx.math.Vector2;
+
+public class AI implements Component, Steerable<Vector2> {
+    public Vector2 linearVelocity;
+    public float angularVelocity;
+    public float boundingRadius;
+    public boolean tagged;
+    public float zeroLinearSpeedThreshold;
+    public float maxLinearSpeed;
+    public float maxLinearAcceleration;
+    public float maxAngularSpeed;
+    public float maxAngularAcceleration;
+    public Vector2 position;
+    public float orientation;
+
+
+    @Override
+    public Vector2 getLinearVelocity() {
+        return linearVelocity;
+    }
+
+    @Override
+    public float getAngularVelocity() {
+        return angularVelocity;
+    }
+
+    @Override
+    public float getBoundingRadius() {
+        return boundingRadius;
+    }
+
+    @Override
+    public boolean isTagged() {
+        return tagged;
+    }
+
+    @Override
+    public void setTagged(boolean tagged) {
+        this.tagged = tagged;
+    }
+
+    @Override
+    public float getZeroLinearSpeedThreshold() {
+        return zeroLinearSpeedThreshold;
+    }
+
+    @Override
+    public void setZeroLinearSpeedThreshold(float value) {
+        zeroLinearSpeedThreshold = value;
+    }
+
+    @Override
+    public float getMaxLinearSpeed() {
+        return maxLinearSpeed;
+    }
+
+    @Override
+    public void setMaxLinearSpeed(float maxLinearSpeed) {
+        this.maxLinearSpeed = maxLinearSpeed;
+    }
+
+    @Override
+    public float getMaxLinearAcceleration() {
+        return maxLinearAcceleration;
+    }
+
+    @Override
+    public void setMaxLinearAcceleration(float maxLinearAcceleration) {
+        this.maxLinearAcceleration = maxLinearAcceleration;
+    }
+
+    @Override
+    public float getMaxAngularSpeed() {
+        return maxAngularSpeed;
+    }
+
+    @Override
+    public void setMaxAngularSpeed(float maxAngularSpeed) {
+        this.maxAngularSpeed = maxAngularSpeed;
+    }
+
+    @Override
+    public float getMaxAngularAcceleration() {
+        return maxAngularAcceleration;
+    }
+
+    @Override
+    public void setMaxAngularAcceleration(float maxAngularAcceleration) {
+        this.maxAngularAcceleration = maxAngularAcceleration;
+    }
+
+    @Override
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    @Override
+    public float getOrientation() {
+        return orientation;
+    }
+
+    @Override
+    public void setOrientation(float orientation) {
+        this.orientation = orientation;
+    }
+
+    @Override
+    public float vectorToAngle(Vector2 vector) {
+        return 0;
+    }
+
+    @Override
+    public Vector2 angleToVector(Vector2 outVector, float angle) {
+        return null;
+    }
+
+    @Override
+    public Location<Vector2> newLocation() {
+        return null;
+    }
+}
