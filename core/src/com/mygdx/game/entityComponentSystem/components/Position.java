3f1194317241cf9b321cec76057c59afcc82967b
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-17 13:30:53 -0700

    add old x,y, Vector2 position

--- core/src/com/mygdx/game/entityComponentSystem/components/Position.java
+++ core/src/com/mygdx/game/entityComponentSystem/components/Position.java
@@ -3,6 +3,7 @@ package com.mygdx.game.entityComponentSystem.components;
 import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.GameMapProperties;
 
 import java.util.Random;
@@ -11,6 +12,10 @@ import java.util.Random;
 public class Position implements Component {
     public float x;
     public float y;
-    public float futureX = x;
-    public float futureY = y;
+    public float oldX;
+    public float oldY;
+    public float futureX;
+    public float futureY;
+    public Vector2 position = new Vector2();
+    public Vector2 oldPosition = new Vector2();
 }
