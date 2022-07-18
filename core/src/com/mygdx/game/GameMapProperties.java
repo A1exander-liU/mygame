13fa0afd7dff85a659a00698ae65f2d5f92ae22e
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-17 18:05:39 -0700

    add static string field from map layer names

--- core/src/com/mygdx/game/GameMapProperties.java
+++ core/src/com/mygdx/game/GameMapProperties.java
@@ -1,8 +1,12 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapLayers;
 import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+
+import java.util.Iterator;
 
 public class GameMapProperties {
     public TiledMap tiledMap;
@@ -10,6 +14,10 @@ public class GameMapProperties {
     public int mapHeight;
     public int tileWidth;
     public int tileHeight;
+    public static String GROUND_LAYER;
+    public static String ENEMY_SPAWNS;
+    public static String COLLISIONS;
+    public static String OBSTACLE_LAYER;
 
     public GameMapProperties(TiledMap tiledMap) {
         this.tiledMap = tiledMap;
@@ -24,6 +32,12 @@ public class GameMapProperties {
         tileHeight = mapProps.get("tileheight", Integer.class);
         mapWidth = xCells * tileWidth;
         mapHeight = yCells * tileHeight;
+        // are in order from bottom most layer to top most layer
+        // order is defined from looking at the actual tiled map layers
+        GROUND_LAYER = tiledMap.getLayers().get(0).getName();
+        ENEMY_SPAWNS = tiledMap.getLayers().get(1).getName();
+        COLLISIONS = tiledMap.getLayers().get(2).getName();
+        OBSTACLE_LAYER = tiledMap.getLayers().get(3).getName();
     }
 
     public MapLayer getMapLayer(String name) {
