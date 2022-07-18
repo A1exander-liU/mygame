4061b8484d1caa708393203d11f2f7b2cc04cf08
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-18 14:36:25 -0700

    add enum states and boolean

--- core/src/com/mygdx/game/entityComponentSystem/components/Enemy.java
+++ core/src/com/mygdx/game/entityComponentSystem/components/Enemy.java
@@ -3,6 +3,11 @@ package com.mygdx.game.entityComponentSystem.components;
 import com.badlogic.ashley.core.Component;
 
 public class Enemy implements Component {
+    // wander: un-aggravated, wandering inside spawn zone
+    // arrive: following the player
+    // flee: moving back to spawn point
+    public enum States {WANDER, ARRIVE, FLEE};
     public boolean spawned = false;
     public boolean isAlive = true;
+    public boolean hunting = false;
 }
