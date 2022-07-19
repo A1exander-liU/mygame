95c0e7b8ce7d985c1cd5da93cf0a9319d1612546
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-19 16:20:12 -0700

    add GameLocation class for the steering behavior to calc new location

---
+++ core/src/com/mygdx/game/GameLocation.java
@@ -0,0 +1,46 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ai.utils.Location;
+import com.badlogic.gdx.math.Vector2;
+
+public class GameLocation implements Location<Vector2> {
+    Vector2 position;
+    float orientation;
+
+    public GameLocation() {
+        this.position = new Vector2();
+        this.orientation = 0;
+    }
+
+    @Override
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    @Override
+    public float getOrientation() {
+        return orientation;
+    }
+
+    @Override
+    public void setOrientation(float orientation) {
+        this.orientation = orientation;
+    }
+
+    @Override
+    public float vectorToAngle(Vector2 vector) {
+        return (float)Math.atan2(-vector.x, vector.y);
+    }
+
+    @Override
+    public Vector2 angleToVector(Vector2 outVector, float angle) {
+        outVector.x = -(float)Math.sin(angle);
+        outVector.y = (float)Math.cos(angle);
+        return outVector;
+    }
+
+    @Override
+    public Location<Vector2> newLocation() {
+        return new GameLocation();
+    }
+}
