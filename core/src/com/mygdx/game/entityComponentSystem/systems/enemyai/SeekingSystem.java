4a297d25a4dd5d050e58d25c70351b796c8c4512
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-19 17:45:10 -0700

    get player entity from entities

--- core/src/com/mygdx/game/entityComponentSystem/systems/enemyai/SeekingSystem.java
+++ core/src/com/mygdx/game/entityComponentSystem/systems/enemyai/SeekingSystem.java
@@ -4,16 +4,25 @@ import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.ai.GdxAI;
+import com.badlogic.gdx.ai.steer.behaviors.Arrive;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.TextureMapObject;
+import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.GameMapProperties;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.entityComponentSystem.ComponentGrabber;
 import com.mygdx.game.entityComponentSystem.Families;
+import com.mygdx.game.entityComponentSystem.components.ID;
+import com.mygdx.game.entityComponentSystem.components.Position;
+import com.mygdx.game.entityComponentSystem.components.Steering;
 
 public class SeekingSystem extends EntitySystem {
     ComponentGrabber cg;
     MyGame root;
     GameMapProperties gameMapProperties;
     ImmutableArray<Entity> enemies;
+    Entity player;
 
     public SeekingSystem(ComponentGrabber cg, MyGame root, GameMapProperties gameMapProperties) {
         super(4);
@@ -21,6 +30,7 @@ public class SeekingSystem extends EntitySystem {
         this.root = root;
         this.gameMapProperties = gameMapProperties;
         enemies = root.engine.getEntitiesFor(Families.enemies);
+        player = root.engine.getEntitiesFor(Families.player).get(0);
     }
 
     @Override
@@ -30,8 +40,5 @@ public class SeekingSystem extends EntitySystem {
 
     @Override
     public void update(float delta) {
-
     }
-
-
 }
