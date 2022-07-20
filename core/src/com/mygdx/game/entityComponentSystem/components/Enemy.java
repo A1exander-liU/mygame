fec3d8c2f0d541bd9697dae92e13059aa563e149
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-20 14:53:25 -0700

    add variable to determine enemy current state

--- core/src/com/mygdx/game/entityComponentSystem/components/Enemy.java
+++ core/src/com/mygdx/game/entityComponentSystem/components/Enemy.java
@@ -4,9 +4,10 @@ import com.badlogic.ashley.core.Component;
 
 public class Enemy implements Component {
     // wander: un-aggravated, wandering inside spawn zone
-    // arrive: following the player
-    // flee: moving back to spawn point
-    public enum States {WANDER, ARRIVE, FLEE};
+    // pursue: following the player
+    // return: moving back to spawn point
+    public enum States {WANDER, PURSUE, RETURN};
+    public States state;
     public boolean spawned = false;
     public boolean isAlive = true;
     public boolean hunting = false;
