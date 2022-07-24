72e853072e8062e43df1a5a3d57c4256162e7cb4
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-24 14:22:00 -0700

    creating entites from obstacles on map

--- core/src/com/mygdx/game/GameMapProperties.java
+++ core/src/com/mygdx/game/GameMapProperties.java
@@ -1,10 +1,21 @@
 package com.mygdx.game;
 
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.utils.ImmutableArray;
 import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.math.Rectangle;
+import com.mygdx.game.entityComponentSystem.Families;
+import com.mygdx.game.entityComponentSystem.components.DetectionProximity;
+import com.mygdx.game.entityComponentSystem.components.ID;
+import com.mygdx.game.entityComponentSystem.components.Position;
+import com.mygdx.game.entityComponentSystem.components.Size;
+import com.mygdx.game.entityComponentSystem.components.Steering;
 
 import java.util.Iterator;
 
@@ -18,10 +29,15 @@ public class GameMapProperties {
     public static String ENEMY_SPAWNS;
     public static String COLLISIONS;
     public static String OBSTACLE_LAYER;
+    MyGame root;
+    // all the collision boxes on the map
+    public ImmutableArray<Entity> staticObstacles;
 
-    public GameMapProperties(TiledMap tiledMap) {
+    public GameMapProperties(TiledMap tiledMap, MyGame root) {
         this.tiledMap = tiledMap;
+        this.root = root;
         setMapProperties();
+        makeEntitiesFromCollisions();
     }
 
     private void setMapProperties() {
@@ -46,5 +62,40 @@ public class GameMapProperties {
 
     public MapLayer getMapLayer(int index) {
         return tiledMap.getLayers().get(index);
+    }
+
+    private void makeEntitiesFromCollisions() {
+        // will only account for non enemies
+        MapObjects collisions = getMapLayer(GameMapProperties.COLLISIONS).getObjects();
+        for (int i = 0; i < collisions.getCount(); i++) {
+            // note: texture map objects are enemies everything else is obstacle
+            // the rectangle map objects are the collision boxes defined initially in the tiled map
+            if (collisions.get(i) instanceof RectangleMapObject) {
+                Rectangle area = ((RectangleMapObject) collisions.get(i)).getRectangle();
+                Entity obstacle = new Entity();
+                obstacle.add(new Position());
+                obstacle.add(new Size());
+                setPosition(obstacle, area);
+                setSize(obstacle, area);
+                obstacle.add(new Steering(obstacle));
+                obstacle.add(new DetectionProximity(obstacle, 20, root));
+                root.engine.addEntity(obstacle);
+            }
+        }
+        staticObstacles = root.engine.getEntitiesFor(Families.obstacles);
+    }
+
+    private void setPosition(Entity entity, Rectangle area) {
+        Position pos = entity.getComponent(Position.class);
+        pos.x = area.x;
+        pos.y = area.y;
+        pos.oldX = area.x;
+        pos.oldY = area.y;
+    }
+
+    private void setSize(Entity entity, Rectangle area) {
+        Size size = entity.getComponent(Size.class);
+        size.width = area.width;
+        size.height = area.height;
     }
 }
