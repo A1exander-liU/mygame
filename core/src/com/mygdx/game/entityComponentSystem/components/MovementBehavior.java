a4c3a51b17468ffa93f33966894bc91397a74f2a
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-25 14:17:14 -0700

    add component to store steering behaviors

---
+++ core/src/com/mygdx/game/entityComponentSystem/components/MovementBehavior.java
@@ -0,0 +1,25 @@
+package com.mygdx.game.entityComponentSystem.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.ai.steer.behaviors.Arrive;
+import com.badlogic.gdx.ai.steer.behaviors.Seek;
+import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.GameLocation;
+
+public class MovementBehavior implements Component {
+    Arrive<Vector2> pursue;
+    Seek<Vector2> back;
+    Seek<Vector2> wander;
+
+    public MovementBehavior(Entity entity) {
+        Steering steering = entity.getComponent(Steering.class);
+        Spawn spawn = entity.getComponent(Spawn.class);
+        pursue = new Arrive<>(steering);
+        back = new Seek<>(steering);
+        wander = new Seek<>(steering);
+        GameLocation spawnPosition = new GameLocation(spawn.spawnPosX, spawn.spawnPosY);
+        wander.setTarget(spawnPosition);
+    }
+
+}
