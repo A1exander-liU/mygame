0f343f4f086f9c6a6f3c8056bf0c312222775be4
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-25 15:43:17 -0700

    remove sout

--- core/src/com/mygdx/game/entityComponentSystem/systems/TimeSystem.java
+++ core/src/com/mygdx/game/entityComponentSystem/systems/TimeSystem.java
@@ -2,14 +2,12 @@ package com.mygdx.game.entityComponentSystem.systems;
 
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.EntitySystem;
-import com.badlogic.gdx.utils.Timer;
 import com.mygdx.game.MyGame;
 
 public class TimeSystem extends EntitySystem {
     MyGame root;
     public static float time = 0;
     public static int second = 0;
-    Timer timer = new Timer();
 
     public TimeSystem(MyGame root) {
         this.root = root;
@@ -22,11 +20,9 @@ public class TimeSystem extends EntitySystem {
 
     @Override
     public void update(float delta) {
-
         time += delta;
         if (time - second >= 1) {
             second++;
-            System.out.println(second);
         }
     }
 
