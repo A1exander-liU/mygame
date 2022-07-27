3f89ede5a1558212f78e952c45bf29cd0c38cfff
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-26 17:54:28 -0700

    add task to set to wander behavior

---
+++ core/src/com/mygdx/game/ai/tasks/enemy/steering/setToWander.java
@@ -0,0 +1,34 @@
+package com.mygdx.game.ai.tasks.enemy.steering;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.ai.btree.LeafTask;
+import com.badlogic.gdx.ai.btree.Task;
+import com.mygdx.game.entityComponentSystem.MobEntity;
+import com.mygdx.game.entityComponentSystem.components.MovementBehavior;
+import com.mygdx.game.entityComponentSystem.components.Steering;
+
+public class setToWander extends LeafTask<MobEntity> {
+    private Entity player;
+
+    public setToWander(Entity player) {
+        this.player = player;
+    }
+
+    @Override
+    public Status execute() {
+        MobEntity enemy = getObject();
+        setToWanderBehavior(enemy);
+        return Status.SUCCEEDED;
+    }
+
+    @Override
+    protected Task<MobEntity> copyTo(Task<MobEntity> task) {
+        return null;
+    }
+
+    private void setToWanderBehavior(Entity entity) {
+        Steering steering = entity.getComponent(Steering.class);
+        MovementBehavior movementBehavior = entity.getComponent(MovementBehavior.class);
+        steering.steeringBehavior = movementBehavior.wander;
+    }
+}
