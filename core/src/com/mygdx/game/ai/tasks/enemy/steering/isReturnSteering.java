c13800ba2c7a089ed4983fb9b806c219806cc2d7
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-26 17:17:17 -0700

    check value of enemy steering behavior

---
+++ core/src/com/mygdx/game/ai/tasks/enemy/steering/isReturnSteering.java
@@ -0,0 +1,31 @@
+package com.mygdx.game.ai.tasks.enemy.steering;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.ai.btree.LeafTask;
+import com.badlogic.gdx.ai.btree.Task;
+import com.mygdx.game.entityComponentSystem.MobEntity;
+import com.mygdx.game.entityComponentSystem.components.MovementBehavior;
+import com.mygdx.game.entityComponentSystem.components.Steering;
+
+public class isReturnSteering extends LeafTask<MobEntity> {
+
+    public isReturnSteering() {}
+
+    @Override
+    public Status execute() {
+        MobEntity enemy = getObject();
+        if (enemySteeringIsReturn(enemy))
+            return Status.SUCCEEDED;
+        return Status.FAILED;
+    }
+
+    @Override
+    protected Task<MobEntity> copyTo(Task<MobEntity> task) {
+        return task;
+    }
+
+    private boolean enemySteeringIsReturn(Entity entity) {
+        return entity.getComponent(Steering.class).steeringBehavior
+                == entity.getComponent(MovementBehavior.class).back;
+    }
+}
