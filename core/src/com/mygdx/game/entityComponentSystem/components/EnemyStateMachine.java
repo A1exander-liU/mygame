70d3e6d124e26dd733cabca4ed57ca1125dc9736
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-30 15:25:25 -0700

    add EnemyStateMachine

---
+++ core/src/com/mygdx/game/entityComponentSystem/components/EnemyStateMachine.java
@@ -0,0 +1,42 @@
+package com.mygdx.game.entityComponentSystem.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.ai.fsm.StackStateMachine;
+import com.mygdx.game.entityComponentSystem.EnemyState;
+import com.mygdx.game.entityComponentSystem.MobEntity;
+
+public class EnemyStateMachine extends StackStateMachine<MobEntity, EnemyState> implements Component {
+    private Entity enemy;
+    StateComponent stateComponent;
+    // provides logic to modify state of entity
+
+    public EnemyStateMachine(Entity entity) {
+        enemy = entity;
+        stateComponent = entity.getComponent(StateComponent.class);
+    }
+
+    @Override
+    public void update() {}
+
+    public void changeState(EnemyState newState) {
+        stateComponent.state = newState;
+    }
+
+    @Override
+    public boolean revertToPreviousState() {return false;}
+
+    @Override
+    public void setInitialState(EnemyState state) {
+        stateComponent.state = state;
+    }
+
+    @Override
+    public void setGlobalState(EnemyState state) {}
+
+    @Override
+    public boolean isInState(EnemyState state) {
+        return stateComponent.state == state;
+    }
+}
+
