6e9d389bece209d99a9e1e2500b97d3e332ef0cf
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-30 16:08:33 -0700

    check if enemy is aggravated

--- core/src/com/mygdx/game/entityComponentSystem/systems/enemyai/StateSystem.java
+++ core/src/com/mygdx/game/entityComponentSystem/systems/enemyai/StateSystem.java
@@ -3,8 +3,14 @@ package com.mygdx.game.entityComponentSystem.systems.enemyai;
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
+import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.ai.fsm.StateMachine;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.math.Rectangle;
 import com.mygdx.game.GameMapProperties;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.entityComponentSystem.ComponentGrabber;
@@ -12,7 +18,12 @@ import com.mygdx.game.entityComponentSystem.EnemyState;
 import com.mygdx.game.entityComponentSystem.Families;
 import com.mygdx.game.entityComponentSystem.MobEntity;
 import com.mygdx.game.entityComponentSystem.components.EnemyStateMachine;
+import com.mygdx.game.entityComponentSystem.components.Position;
+import com.mygdx.game.entityComponentSystem.components.Size;
+import com.mygdx.game.entityComponentSystem.components.Spawn;
 import com.mygdx.game.entityComponentSystem.components.StateComponent;
+
+import javax.swing.SizeSequence;
 
 
 public class StateSystem extends EntitySystem {
@@ -20,12 +31,14 @@ public class StateSystem extends EntitySystem {
     GameMapProperties gameMapProperties;
 
     ImmutableArray<Entity> enemies;
+    Entity player;
 
     public StateSystem(ComponentGrabber cg, GameMapProperties gameMapProperties) {
         super(5);
         this.cg = cg;
         this.gameMapProperties = gameMapProperties;
         enemies = MyGame.engine.getEntitiesFor(Families.enemies);
+        player = MyGame.engine.getEntitiesFor(Families.player).get(0);
         setStateToIdle();
     }
 
@@ -38,8 +51,7 @@ public class StateSystem extends EntitySystem {
     public void update(float delta) {
         for (int i = 0; i < enemies.size(); i++) {
             Entity entity = enemies.get(i);
-            // update the state of entity depending on logic
-            //IDLE: all enemies are initially wandering
+
         }
     }
 
@@ -49,5 +61,33 @@ public class StateSystem extends EntitySystem {
             EnemyStateMachine stateMachine = cg.getStateMachine(entity);
             stateMachine.setInitialState(EnemyState.IDLE);
         }
+    }
+
+    private boolean enemyAggravated(Entity entity) {
+        if (cg.getStateMachine(entity).getCurrentState() == EnemyState.IDLE) {
+            Rectangle enemySpawn = buildSpawnArea(entity);
+            Rectangle playerRect = buildEntityArea(player);
+            return playerRect.overlaps(enemySpawn);
+        }
+        return false;
+    }
+
+    private Rectangle buildSpawnArea(Entity entity) {
+        Spawn enemy = cg.getSpawn(entity);
+        MapObjects spawns = gameMapProperties.getMapLayer(GameMapProperties.ENEMY_SPAWNS).getObjects();
+        for (int i = 0; i < spawns.getCount(); i++) {
+            Rectangle spawn = ((RectangleMapObject) spawns.get(i)).getRectangle();
+            float spawnX = spawn.x + (spawn.width / 2);
+            float spawnY = spawn.y + (spawn.height / 2);
+            if (spawnX == enemy.spawnPosX && spawnY == enemy.spawnPosY)
+                return spawn;
+        }
+        return null;
+    }
+
+    private Rectangle buildEntityArea(Entity entity) {
+        Position pos = cg.getPosition(entity);
+        Size size = cg.getSize(entity);
+        return new Rectangle(pos.x, pos.y, size.width, size.height);
     }
 }
