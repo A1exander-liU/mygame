5f8366ce419562c982ea63bcfc480bcee04befef
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-30 15:47:15 -0700

    change type to EnemyState

--- core/src/com/mygdx/game/entityComponentSystem/components/StateComponent.java
+++ core/src/com/mygdx/game/entityComponentSystem/components/StateComponent.java
@@ -2,18 +2,19 @@ package com.mygdx.game.entityComponentSystem.components;
 
 import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.ai.fsm.State;
+import com.mygdx.game.entityComponentSystem.EnemyState;
 import com.mygdx.game.entityComponentSystem.MobEntity;
 
 // state component is state only
 // have system to handle the update logic of changing state of the agent
 public class StateComponent implements Component {
-    public State<MobEntity> state;
+    public EnemyState state;
 
     public StateComponent() {
 
     }
 
-    public StateComponent(State<MobEntity> state) {
+    public StateComponent(EnemyState state) {
         this.state = state;
     }
 }
