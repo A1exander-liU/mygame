529405012deef129d324e0c213245eec64bec990
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-31 16:06:04 -0700

    add parameter component

---
+++ core/src/com/mygdx/game/engine/components/ParameterComponent.java
@@ -0,0 +1,8 @@
+package com.mygdx.game.engine.components;
+
+import com.badlogic.ashley.core.Component;
+
+public class ParameterComponent implements Component {
+    public Health health;
+    public int damage;
+}
