db96878fddf92b41b0541ca6c6be1697d1f59f44
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-31 15:53:49 -0700

    create JsonSearcher add method to search json file and get enemy by name

---
+++ core/src/com/mygdx/game/JsonSearcher.java
@@ -0,0 +1,36 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
+
+import java.util.Objects;
+
+//JsonReader jsonReader = new JsonReader();
+//        JsonValue base = jsonReader.parse(Gdx.files.internal("enemies.json"));
+//        JsonValue enemies = base.get("enemies");
+//        for (JsonValue enemy : enemies) {
+//        System.out.println(enemy.getString("name"));
+//        }
+
+public class JsonSearcher extends JsonReader {
+    private final JsonValue enemies;
+
+    public JsonSearcher(FileHandle fileHandle) {
+        super();
+        enemies = super.parse(fileHandle).get("enemies");
+    }
+
+    public JsonValue getEnemies() {
+        return enemies;
+    }
+
+    public JsonValue findByEnemyName(String name) {
+        // search and find object based on enemy name
+        for (JsonValue enemy : enemies) {
+            if (Objects.equals(enemy.getString("name"), name))
+                return enemy;
+        }
+        return null;
+    }
+}
