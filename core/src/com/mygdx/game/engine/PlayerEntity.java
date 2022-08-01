eda1e3cba8fb1cac195ae5428ec8496319b98d29
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-31 17:02:12 -0700

    reference with class instead of object

--- core/src/com/mygdx/game/engine/PlayerEntity.java
+++ core/src/com/mygdx/game/engine/PlayerEntity.java
@@ -35,7 +35,7 @@ public class PlayerEntity extends Entity {
         Position pos = cg.getPosition(this);
         Size size = cg.getSize(this);
         playerBox = new Rectangle(pos.x, pos.y, size.width, size.height);
-        root.engine.addEntity(this);
+        MyGame.engine.addEntity(this);
         EntityToMapAdder entityToMapAdder = new EntityToMapAdder(gameMapProperties.tiledMap, cg);
         root.entityToMapAdder.addEntityToMap(this);
     }
