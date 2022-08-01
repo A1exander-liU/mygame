2fd88626287262c806baa1dcc6cfb4cdad6ccfbf
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-01 13:56:09 -0700

    add new constructor

--- core/src/com/mygdx/game/utils/EntityTextureObject.java
+++ core/src/com/mygdx/game/utils/EntityTextureObject.java
@@ -7,11 +7,16 @@ import com.badlogic.gdx.maps.objects.TextureMapObject;
 public class EntityTextureObject extends TextureMapObject {
     private Entity owner;
 
+    public EntityTextureObject() {}
+
     public EntityTextureObject(TextureRegion textureRegion) {
         super(textureRegion);
     }
 
-    public EntityTextureObject() {}
+    public EntityTextureObject(TextureRegion textureRegion, Entity owner) {
+        super(textureRegion);
+        this.owner = owner;
+    }
 
     public Entity getOwner() {
         return owner;
