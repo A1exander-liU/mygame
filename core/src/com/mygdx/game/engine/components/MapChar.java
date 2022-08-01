dedc92e16b6f8c53af0ecee49c4b8ee713c3aeb4
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-31 18:16:15 -0700

    add MapChar component

---
+++ core/src/com/mygdx/game/engine/components/MapChar.java
@@ -0,0 +1,12 @@
+package com.mygdx.game.engine.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.objects.TextureMapObject;
+
+public class MapChar extends TextureMapObject implements Component {
+
+    public MapChar(TextureRegion textureRegion) {
+        super(textureRegion);
+    }
+}
