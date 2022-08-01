1360001983ed5e448c55b493d188f12f46320921
AlexanderLiu <alexanderliu2016@gmail.com>
2022-07-31 18:21:13 -0700

    add new empty contructor

--- core/src/com/mygdx/game/engine/components/MapChar.java
+++ core/src/com/mygdx/game/engine/components/MapChar.java
@@ -6,6 +6,10 @@ import com.badlogic.gdx.maps.objects.TextureMapObject;
 
 public class MapChar extends TextureMapObject implements Component {
 
+    public MapChar() {
+
+    }
+
     public MapChar(TextureRegion textureRegion) {
         super(textureRegion);
     }
