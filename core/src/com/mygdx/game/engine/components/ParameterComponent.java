9a13ac76e2af42aab44bb719aad6702cbf3166ce
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-01 14:06:47 -0700

    add constructor to initialize health component

--- core/src/com/mygdx/game/engine/components/ParameterComponent.java
+++ core/src/com/mygdx/game/engine/components/ParameterComponent.java
@@ -5,4 +5,8 @@ import com.badlogic.ashley.core.Component;
 public class ParameterComponent implements Component {
     public Health health;
     public int damage;
+
+    public ParameterComponent() {
+        health = new Health();
+    }
 }
