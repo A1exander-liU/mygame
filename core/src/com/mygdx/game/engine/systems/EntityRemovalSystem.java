40cfd603de6a3e7a6c9fc7c770e9caae0c13293d
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-01 16:07:24 -0700

    remove enemy from map

--- core/src/com/mygdx/game/engine/systems/EntityRemovalSystem.java
+++ core/src/com/mygdx/game/engine/systems/EntityRemovalSystem.java
@@ -4,18 +4,23 @@ import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.maps.MapObjects;
+import com.mygdx.game.GameMapProperties;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.engine.ComponentGrabber;
 import com.mygdx.game.engine.Families;
 import com.mygdx.game.engine.components.Enemy;
+import com.mygdx.game.utils.EntityTextureObject;
 
 public class EntityRemovalSystem extends EntitySystem {
     ComponentGrabber cg;
+    GameMapProperties gameMapProperties;
     ImmutableArray<Entity> enemies;
 
-    public EntityRemovalSystem(ComponentGrabber cg) {
+    public EntityRemovalSystem(ComponentGrabber cg, GameMapProperties gameMapProperties) {
         super(9);
         this.cg = cg;
+        this.gameMapProperties = gameMapProperties;
     }
 
     @Override
@@ -29,13 +34,28 @@ public class EntityRemovalSystem extends EntitySystem {
         for (int i = 0; i < enemies.size(); i++) {
             Entity entity = enemies.get(i);
             Enemy enemy = cg.getEnemy(entity);
-            if (!enemy.isAlive)
+            if (!enemy.isAlive) {
                 MyGame.engine.removeEntity(entity);
+                removeFromMap(entity);
+            }
         }
     }
 
     private ImmutableArray<Entity> getEnemies() {
         return MyGame.engine.getEntitiesFor(Families.enemies);
+    }
+
+    private void removeFromMap(Entity entity) {
+        MapObjects collisions = gameMapProperties.getMapLayer(GameMapProperties.COLLISIONS).getObjects();
+        for (int i = 0; i < collisions.getCount(); i++) {
+            if (collisions.get(i) instanceof EntityTextureObject) {
+                EntityTextureObject textureObject = (EntityTextureObject) collisions.get(i);
+                if (textureObject.getOwner() == entity) {
+                    collisions.remove(collisions.get(i));
+                    break;
+                }
+            }
+        }
     }
 
 }
