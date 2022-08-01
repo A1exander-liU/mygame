90cf8dc2dfa8f9dc64be34522c38304370915ced
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-01 16:45:00 -0700

    update families add new spawn familiy and update obstacles family

--- core/src/com/mygdx/game/engine/Families.java
+++ core/src/com/mygdx/game/engine/Families.java
@@ -3,14 +3,15 @@ package com.mygdx.game.engine;
 import com.badlogic.ashley.core.Family;
 import com.mygdx.game.engine.components.Enemy;
 import com.mygdx.game.engine.components.Player;
+import com.mygdx.game.engine.components.SpawnArea;
 
 public class Families {
 
     public static final Family enemies = Family.all(Enemy.class).get();
     public static final Family player = Family.all(Player.class).get();
-    public static final Family obstacles = Family.exclude(Enemy.class, Player.class).get();
+    public static final Family obstacles = Family.exclude(Enemy.class, Player.class, SpawnArea.class).get();
+    public static final Family spawns = Family.all(SpawnArea.class).get();
 
     public Families() {}
-
-
+    
 }
