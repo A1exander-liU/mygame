4c3ec736502e1980948d6b9d411dcf8d09e1de67
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-01 16:29:25 -0700

    make spawn area component

---
+++ core/src/com/mygdx/game/engine/components/SpawnArea.java
@@ -0,0 +1,11 @@
+package com.mygdx.game.engine.components;
+
+import com.badlogic.ashley.core.Entity;
+
+public class SpawnArea {
+    public Entity owner;
+    public float xCenter;
+    public float yCenter;
+
+    public SpawnArea() {}
+}
