786a5eb3a23bda0a15c824b559849ad32df63a49
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-02 15:37:47 -0700

    adding each entity has an item to world

--- core/src/com/mygdx/game/engine/systems/SimulationSystem.java
+++ core/src/com/mygdx/game/engine/systems/SimulationSystem.java
@@ -3,11 +3,24 @@ package com.mygdx.game.engine.systems;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.ashley.utils.ImmutableArray;
+import com.dongbat.jbump.Collision;
+import com.dongbat.jbump.CollisionFilter;
+import com.dongbat.jbump.Item;
+import com.dongbat.jbump.Response;
 import com.dongbat.jbump.World;
 import com.mygdx.game.GameMapProperties;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.engine.ComponentGrabber;
 import com.mygdx.game.engine.Families;
+import com.mygdx.game.engine.components.Position;
+import com.mygdx.game.engine.components.Size;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.print.attribute.SetOfIntegerSyntax;
+
+import jdk.javadoc.internal.doclets.toolkit.taglets.SeeTaglet;
 
 public class SimulationSystem extends EntitySystem {
     ComponentGrabber cg;
@@ -24,6 +37,15 @@ public class SimulationSystem extends EntitySystem {
         enemies = MyGame.engine.getEntitiesFor(Families.enemies);
         player = MyGame.engine.getEntitiesFor(Families.player).get(0);
         world = new World<>();
+        for (int i = 0; i < collisions.size(); i++) {
+            Entity entity = collisions.get(i);
+            entity.add(new com.mygdx.game.engine.components.Item());
+            Position pos = cg.getPosition(entity);
+            Size size = cg.getSize(entity);
+            Item<Entity> item = new Item<>(entity);
+            cg.getItem(entity).item = item;
+            world.add(item, pos.x, pos.y, size.width, size.height);
+        }
     }
 
     @Override
