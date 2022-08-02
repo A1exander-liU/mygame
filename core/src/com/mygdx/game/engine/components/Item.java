af1f72bd5605bef5aae0029f914f9ab9186bbd7a
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-02 15:35:11 -0700

    add item component

---
+++ core/src/com/mygdx/game/engine/components/Item.java
@@ -0,0 +1,8 @@
+package com.mygdx.game.engine.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.Entity;
+
+public class Item implements Component {
+    public com.dongbat.jbump.Item<Entity> item;
+}
