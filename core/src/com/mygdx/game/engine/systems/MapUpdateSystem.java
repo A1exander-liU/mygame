9ffb5a5496e3796a18e23021dfa337f0c210aae6
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-02 17:00:59 -0700

    find and update char position in map

--- core/src/com/mygdx/game/engine/systems/MapUpdateSystem.java
+++ core/src/com/mygdx/game/engine/systems/MapUpdateSystem.java
@@ -4,11 +4,14 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.maps.MapObjects;
 import com.mygdx.game.GameMapProperties;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.engine.ComponentGrabber;
 import com.mygdx.game.engine.components.Enemy;
 import com.mygdx.game.engine.components.Player;
+import com.mygdx.game.engine.components.Position;
+import com.mygdx.game.utils.EntityTextureObject;
 
 public class MapUpdateSystem extends EntitySystem {
     ComponentGrabber cg;
@@ -25,6 +28,32 @@ public class MapUpdateSystem extends EntitySystem {
     public void update(float delta) {
         for (int i = 0; i < characters.size(); i++) {
             Entity entity = characters.get(i);
+            updateEntityInMap(entity);
         }
+    }
+
+    private void updateEntityInMap(Entity entity) {
+        Position pos = cg.getPosition(entity);
+        EntityTextureObject textureObject = findSameOwner(entity);
+        if (textureObject != null) {
+            textureObject.setX(pos.x);
+            textureObject.setY(pos.y);
+        }
+    }
+
+    private EntityTextureObject findSameOwner(Entity entity) {
+        MapObjects collisions = gameMapProperties.getMapLayer(GameMapProperties.COLLISIONS).getObjects();
+        // going through each item in collision layer
+        // check if it is EntityTextureMap object
+        // check if the owner of it is same as the owner we are trying to find the texture for
+        for (int i = 0; i < collisions.getCount(); i++) {
+            // EntityTextureObject is a character
+            if (collisions.get(i) instanceof EntityTextureObject) {
+                EntityTextureObject textureObject = (EntityTextureObject) collisions.get(i);
+                if (textureObject.getOwner() == entity)
+                    return textureObject;
+            }
+        }
+        return null;
     }
 }
