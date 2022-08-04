60871e5638ae0268bd5018ee161774663ec7a98b
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-04 16:37:15 -0700

    check if player moved

--- core/src/com/mygdx/game/engine/systems/OrientationSystem.java
+++ core/src/com/mygdx/game/engine/systems/OrientationSystem.java
@@ -6,6 +6,7 @@ import com.badlogic.ashley.utils.ImmutableArray;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.engine.ComponentGrabber;
 import com.mygdx.game.engine.Families;
+import com.mygdx.game.engine.components.Position;
 
 public class OrientationSystem extends EntitySystem {
     ComponentGrabber cg;
@@ -21,6 +22,17 @@ public class OrientationSystem extends EntitySystem {
 
     @Override
     public void update(float delta) {
+        updatePlayerOrientation();
+    }
 
+    private void updatePlayerOrientation() {
+        Position pos = cg.getPosition(player);
+        if (playerMoved(pos)) {
+            
+        }
+    }
+
+    private boolean playerMoved(Position pos) {
+        return pos.oldX != pos.x || pos.oldY != pos.y;
     }
 }
