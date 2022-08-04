1ad33deeffc81c861333c8e982adb82d6d6681ca
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-03 17:11:26 -0700

    query world for collisions against ray create custom collision filter to not check collision against the owner of the ray

--- core/src/com/mygdx/game/screens/GameRaycastCollisionDetector.java
+++ core/src/com/mygdx/game/screens/GameRaycastCollisionDetector.java
@@ -1,20 +1,30 @@
 package com.mygdx.game.screens;
 
+import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.ai.utils.Collision;
 import com.badlogic.gdx.ai.utils.Ray;
 import com.badlogic.gdx.ai.utils.RaycastCollisionDetector;
 import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.math.Polyline;
 import com.badlogic.gdx.math.Vector2;
+import com.dongbat.jbump.CollisionFilter;
+import com.dongbat.jbump.Item;
+import com.dongbat.jbump.ItemInfo;
+import com.dongbat.jbump.Response;
 import com.mygdx.game.GameMapProperties;
 import com.mygdx.game.MyGame;
+import com.mygdx.game.engine.MobEntity;
 import com.mygdx.game.engine.systems.CollisionSystem;
+
+import java.util.ArrayList;
 
 public class GameRaycastCollisionDetector implements RaycastCollisionDetector<Vector2> {
     GameMapProperties gameMapProperties;
+    Entity owner;
 
-    public GameRaycastCollisionDetector(GameMapProperties gameMapProperties) {
+    public GameRaycastCollisionDetector(GameMapProperties gameMapProperties, Entity owner) {
         this.gameMapProperties = gameMapProperties;
+        this.owner = owner;
     }
 
     @Override
@@ -29,14 +39,32 @@ public class GameRaycastCollisionDetector implements RaycastCollisionDetector<Ve
         // other enemies
         // obstacles
         // loop through collision layer and check overlaps
-        return thereIsCollision();
+        return thereIsCollision(outputCollision, inputRay);
     }
 
-    private boolean thereIsCollision() {
-        MapObjects collisions = gameMapProperties.getMapLayer(GameMapProperties.COLLISIONS).getObjects();
-        for (int i = 0; i < collisions.getCount(); i++) {
+    private boolean thereIsCollision(Collision<Vector2> outputCollision, Ray<Vector2> ray) {
+        ArrayList<ItemInfo> items = new ArrayList<>();
+        Vector2 start = ray.start;
+        Vector2 end = ray.end;
 
-        }
-        return false;
+        CollisionFilter rayFilter = new CollisionFilter() {
+            @Override
+            public Response filter(Item item, Item other) {
+                // check if it is checking against an enemy
+                if (item.userData instanceof MobEntity) {
+                    // make sure it doesn't do collision with owner of the ray
+                    if (item.userData == owner)
+                        return null;
+                    return Response.cross;
+                }
+                return null;
+            }
+        };
+
+        // query the world for anything that collides with the given ray
+        items = CollisionSystem.world.queryRayWithCoords(
+                start.x, start.y, end.x, end.y,
+                rayFilter,
+                items);
     }
 }
