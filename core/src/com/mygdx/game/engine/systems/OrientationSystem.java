66abc3613762e334beda3dba993e77819931238c
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-04 16:54:02 -0700

    check if both vectors have same value

--- core/src/com/mygdx/game/engine/systems/OrientationSystem.java
+++ core/src/com/mygdx/game/engine/systems/OrientationSystem.java
@@ -47,10 +47,16 @@ public class OrientationSystem extends EntitySystem {
 
     private Vector2 calcBaseDirection(Vector2 direction) {
         Vector2 baseDirection = new Vector2();
+        // divide by the absolute value of itself so the value does not change
+        // so you just end up with 0, -1, or 1
         if (direction.x != 0)
             baseDirection.x = direction.x / Math.abs(direction.x);
         if (direction.y != 0)
             baseDirection.y = direction.y / Math.abs(direction.y);
         return baseDirection;
+    }
+
+    private boolean vectorsEqual(Vector2 a, Vector2 b) {
+        return a.x == b.x && a.y == b.y;
     }
 }
