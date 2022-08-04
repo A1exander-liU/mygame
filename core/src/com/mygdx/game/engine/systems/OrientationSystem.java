d10f4b1c650563bbe0bda49316d7b52120b29958
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-04 16:18:27 -0700

    start orientation system

---
+++ core/src/com/mygdx/game/engine/systems/OrientationSystem.java
@@ -0,0 +1,25 @@
+package com.mygdx.game.engine.systems;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.EntitySystem;
+import com.badlogic.ashley.utils.ImmutableArray;
+import com.mygdx.game.MyGame;
+import com.mygdx.game.engine.ComponentGrabber;
+import com.mygdx.game.engine.Families;
+
+public class OrientationSystem extends EntitySystem {
+    ComponentGrabber cg;
+    ImmutableArray<Entity> enemies;
+    Entity player;
+
+    public OrientationSystem(ComponentGrabber cg) {
+        this.cg = cg;
+        enemies = MyGame.engine.getEntitiesFor(Families.enemies);
+        player = MyGame.engine.getEntitiesFor(Families.player).get(0);
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
+}
