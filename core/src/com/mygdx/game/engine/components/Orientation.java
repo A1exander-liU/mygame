e49163a1f1c73f5c9b9d53df22af1be170a126d7
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-04 16:32:05 -0700

    add field

--- core/src/com/mygdx/game/engine/components/Orientation.java
+++ core/src/com/mygdx/game/engine/components/Orientation.java
@@ -1,2 +1,8 @@
-package com.mygdx.game.engine.components;public class Orientation {
+package com.mygdx.game.engine.components;
+
+import com.badlogic.ashley.core.Component;
+import com.mygdx.game.engine.Direction;
+
+public class Orientation implements Component {
+    public Direction orientation;
 }
