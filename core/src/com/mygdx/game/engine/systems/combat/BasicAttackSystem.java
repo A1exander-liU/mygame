ec0f64cb64e62d68dd93aa028937fac2280d1074
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-05 14:11:36 -0700

    initialize basic attack system

--- core/src/com/mygdx/game/engine/systems/combat/BasicAttackSystem.java
+++ core/src/com/mygdx/game/engine/systems/combat/BasicAttackSystem.java
@@ -1,2 +1,25 @@
-package com.mygdx.game.engine.systems.combat;public class BasicAttackSystem {
+package com.mygdx.game.engine.systems.combat;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.EntitySystem;
+import com.mygdx.game.GameMapProperties;
+import com.mygdx.game.MyGame;
+import com.mygdx.game.engine.ComponentGrabber;
+import com.mygdx.game.engine.Families;
+
+public class BasicAttackSystem extends EntitySystem {
+    ComponentGrabber cg;
+    GameMapProperties gameMapProperties;
+    Entity player;
+
+    public BasicAttackSystem(ComponentGrabber cg, GameMapProperties gameMapProperties) {
+        this.cg = cg;
+        this.gameMapProperties = gameMapProperties;
+        player = MyGame.engine.getEntitiesFor(Families.player).get(0);
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
 }
