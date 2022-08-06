05de30eb4a4e44a34360e8c113eeab3c08579aeb
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-06 15:08:48 -0700

    remove unused constructor and method

--- core/src/com/mygdx/game/engine/PlayerEntity.java
+++ core/src/com/mygdx/game/engine/PlayerEntity.java
@@ -29,20 +29,6 @@ public class PlayerEntity extends Entity {
 
     Rectangle playerBox;
 
-    public PlayerEntity(ComponentGrabber cg, MyGame root, GameMapProperties gameMapProperties, String name) {
-        this.cg = cg;
-        this.root = root;
-        this.gameMapProperties = gameMapProperties;
-        addRequiredComponents();
-        modifyComponentValues(name);
-        Position pos = cg.getPosition(this);
-        Size size = cg.getSize(this);
-        playerBox = new Rectangle(pos.x, pos.y, size.width, size.height);
-        MyGame.engine.addEntity(this);
-        EntityToMapAdder entityToMapAdder = new EntityToMapAdder(gameMapProperties.tiledMap, cg);
-        root.entityToMapAdder.addEntityToMap(this);
-    }
-
     public PlayerEntity() {
         addRequiredComponents();
     }
@@ -62,29 +48,5 @@ public class PlayerEntity extends Entity {
         super.add(new Speed());
         super.add(new Sprite());
         super.add(new Steering(this));
-    }
-
-    private void modifyComponentValues(String playerName) {
-        cg.getSprite(this).texture = new Texture(Gdx.files.internal("testPlayer.png"));
-        cg.getHealth(this).maxHealth = 100;
-        cg.getHealth(this).currentHealth = 100;
-        cg.getName(this).name = playerName;
-        cg.getPosition(this).x = 200;
-        cg.getPosition(this).y = 200;
-        cg.getPosition(this).oldX = cg.getPosition(this).x;
-        cg.getPosition(this).oldY = cg.getPosition(this).y;
-        cg.getPosition(this).position.x = cg.getPosition(this).x;
-        cg.getPosition(this).position.y = cg.getPosition(this).y;
-        cg.getPosition(this).oldPosition.x = cg.getPosition(this).x;
-        cg.getPosition(this).oldPosition.y = cg.getPosition(this).y;
-        cg.getSize(this).width = 32;
-        cg.getSize(this).height = 32;
-        cg.getSpeed(this).xSpeed = 5;
-        cg.getSpeed(this).ySpeed = 5;
-        Camera player = cg.getCamera(this);
-        player.camera = new OrthographicCamera();
-        player.camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        player.camera.position.x = Gdx.graphics.getWidth() / 2f;
-        player.camera.position.y = Gdx.graphics.getHeight() / 2f;
     }
 }
