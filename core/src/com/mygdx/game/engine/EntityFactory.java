3c608b6d746cbb4fd34c18a40fe15da56d296442
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-05 17:40:29 -0700

    make method to modify player component values

--- core/src/com/mygdx/game/engine/EntityFactory.java
+++ core/src/com/mygdx/game/engine/EntityFactory.java
@@ -2,12 +2,16 @@ package com.mygdx.game.engine;
 
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.JsonValue;
 import com.mygdx.game.MyGame;
+import com.mygdx.game.engine.components.Camera;
 import com.mygdx.game.engine.components.EnemyStateMachine;
+import com.mygdx.game.engine.components.Health;
 import com.mygdx.game.engine.components.Name;
 import com.mygdx.game.engine.components.ParameterComponent;
+import com.mygdx.game.engine.components.Position;
 import com.mygdx.game.engine.components.Size;
 import com.mygdx.game.engine.components.Speed;
 import com.mygdx.game.engine.components.Sprite;
@@ -36,7 +40,7 @@ public class EntityFactory {
 
     public void makePlayer(String name) {
         PlayerEntity player = new PlayerEntity();
-
+        modifyPlayerComponentValues(player, name);
     }
 
     private void modifyEnemyComponentValues(Entity entity, String name) {
@@ -53,6 +57,32 @@ public class EntityFactory {
         entitySprite.texture = new Texture(Gdx.files.internal("testPlayer.png"));
         stateMachine.setInitialState(EnemyState.IDLE);
         fillParameterValues(entity, name);
+    }
+
+    private void modifyPlayerComponentValues(Entity entity, String name) {
+        Name playerName = cg.getName(entity);
+        Camera playerCamera = cg.getCamera(entity);
+        Health health = cg.getHealth(entity);
+        Position pos = cg.getPosition(entity);
+        Size size = cg.getSize(entity);
+        Speed speed = cg.getSpeed(entity);
+        Sprite sprite = cg.getSprite(entity);
+        playerCamera.camera = new OrthographicCamera();
+        playerCamera.camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        playerCamera.camera.position.x = Gdx.graphics.getWidth() / 2f;
+        playerCamera.camera.position.y = Gdx.graphics.getHeight() / 2f;
+        playerName.name = name;
+        health.maxHealth = 10;
+        health.currentHealth = 10;
+        pos.x = 200;
+        pos.oldX = 200;
+        pos.y = 200;
+        pos.oldY = 200;
+        size.width = 32;
+        size.height = 32;
+        speed.xSpeed = 5;
+        speed.ySpeed = 5;
+        sprite.texture = new Texture(Gdx.files.internal("testPlayer.png"));
     }
 
     private void fillParameterValues(Entity entity, String name) {
