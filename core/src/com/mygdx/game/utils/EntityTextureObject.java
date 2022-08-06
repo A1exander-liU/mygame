3ba357dc92da8b38690bfc3e7090f555c71b39f0
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-06 15:21:58 -0700

    remove unused constructors

--- core/src/com/mygdx/game/utils/EntityTextureObject.java
+++ core/src/com/mygdx/game/utils/EntityTextureObject.java
@@ -7,12 +7,6 @@ import com.badlogic.gdx.maps.objects.TextureMapObject;
 public class EntityTextureObject extends TextureMapObject {
     private Entity owner;
 
-    public EntityTextureObject() {}
-
-    public EntityTextureObject(TextureRegion textureRegion) {
-        super(textureRegion);
-    }
-
     public EntityTextureObject(TextureRegion textureRegion, Entity owner) {
         super(textureRegion);
         this.owner = owner;
