6fde847c8f9acb3485d16dbaf439221cf1315817
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-05 17:24:46 -0700

    make method to fill parameters

--- core/src/com/mygdx/game/engine/EntityFactory.java
+++ core/src/com/mygdx/game/engine/EntityFactory.java
@@ -3,9 +3,12 @@ package com.mygdx.game.engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.JsonValue;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.engine.components.EnemyStateMachine;
+import com.mygdx.game.engine.components.Health;
 import com.mygdx.game.engine.components.Name;
+import com.mygdx.game.engine.components.ParameterComponent;
 import com.mygdx.game.engine.components.Size;
 import com.mygdx.game.engine.components.Speed;
 import com.mygdx.game.engine.components.Sprite;
@@ -16,6 +19,7 @@ public class EntityFactory {
 
     public EntityFactory(ComponentGrabber cg, MyGame root) {
         this.cg = cg;
+        this.root = root;
     }
 
     // also need method to create a player character
@@ -42,9 +46,14 @@ public class EntityFactory {
         speed.ySpeed = 2;
         entitySprite.texture = new Texture(Gdx.files.internal("testPlayer.png"));
         stateMachine.setInitialState(EnemyState.IDLE);
+        fillParameterValues(entity, name);
     }
 
     private void fillParameterValues(Entity entity, String name) {
-
+        JsonValue enemy = root.jsonSearcher.findByEnemyName(name);
+        ParameterComponent paramCom = cg.getParameters(entity);
+        paramCom.health.maxHealth = enemy.getInt("HP");
+        paramCom.health.currentHealth = enemy.getInt("HP");
+        paramCom.damage = enemy.getInt("DMG");
     }
 }
