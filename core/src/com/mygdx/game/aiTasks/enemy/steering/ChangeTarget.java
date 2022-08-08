1d26ba932bb58a243d1d4db32b73924d66fc8799
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-08 14:12:51 -0700

    access spawns with class reference directly

--- core/src/com/mygdx/game/aiTasks/enemy/steering/ChangeTarget.java
+++ core/src/com/mygdx/game/aiTasks/enemy/steering/ChangeTarget.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.GameMapProperties;
 import com.mygdx.game.utils.GameLocation;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.engine.MobEntity;
@@ -74,7 +75,7 @@ public class ChangeTarget extends LeafTask<MobEntity> {
     private Rectangle getSpawnArea(Spawn spawn) {
         float enemySpawnX = spawn.spawnPosX;
         float enemySpawnY = spawn.spawnPosY;
-        MapObjects spawns = MyGame.gameMapProperties.getMapLayer(MyGame.gameMapProperties.ENEMY_SPAWNS).getObjects();
+        MapObjects spawns = MyGame.gameMapProperties.getMapLayer(GameMapProperties.ENEMY_SPAWNS).getObjects();
         for (int i = 0; i < spawns.getCount(); i++) {
             Rectangle spawnArea = ((RectangleMapObject) spawns.get(i)).getRectangle();
             float spawnCenterX = spawnArea.x + (spawnArea.width / 2);
