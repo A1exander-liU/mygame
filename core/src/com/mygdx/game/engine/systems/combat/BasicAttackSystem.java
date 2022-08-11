71da95d115a3642969cf9b9e03d6e59d7bec4961
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-11 16:15:23 -0700

    check if enemy was hit by ray

--- core/src/com/mygdx/game/engine/systems/combat/BasicAttackSystem.java
+++ core/src/com/mygdx/game/engine/systems/combat/BasicAttackSystem.java
@@ -2,10 +2,14 @@ package com.mygdx.game.engine.systems.combat;
 
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
+import com.badlogic.ashley.utils.ImmutableArray;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ai.utils.Ray;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.collision.BoundingBox;
 import com.mygdx.game.GameMapProperties;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.engine.ComponentGrabber;
@@ -20,12 +24,14 @@ public class BasicAttackSystem extends EntitySystem {
     ComponentGrabber cg;
     GameMapProperties gameMapProperties;
     Entity player;
+    ImmutableArray<Entity> enemies;
 
     public BasicAttackSystem(ComponentGrabber cg, GameMapProperties gameMapProperties) {
         super(8);
         this.cg = cg;
         this.gameMapProperties = gameMapProperties;
         player = MyGame.engine.getEntitiesFor(Families.player).get(0);
+        enemies = MyGame.engine.getEntitiesFor(Families.enemies);
     }
 
     @Override
@@ -41,12 +47,17 @@ public class BasicAttackSystem extends EntitySystem {
         Size size = cg.getSize(player);
         Entity attack = makeAttackAreaEntity();
         AttackRange attackRange = cg.getAttackRange(attack);
+        attackRange.range = 16;
         if (orientation.orientation == Direction.EAST) {
             Vector2 start = new Vector2(
                     pos.x + size.width,
                     pos.y + (size.height / 2));
             Vector2 end = new Vector2(start.x + attackRange.range, start.y);
             Ray<Vector2> ray = new Ray<>(start, end);
+            if (attackLanded(ray)) {
+                // do the dmg and update the health  here
+                
+            }
         }
     }
 
@@ -55,4 +66,24 @@ public class BasicAttackSystem extends EntitySystem {
                 .add(new Position())
                 .add(new AttackRange());
     }
+
+    private boolean attackLanded(Ray<Vector2> attackRay) {
+        for (int i = 0; i < enemies.size(); i++) {
+            Entity entity = enemies.get(i);
+            return checkCollisions(attackRay, entity);
+        }
+
+        return false;
+    }
+
+    private boolean checkCollisions(Ray<Vector2> ray, Entity entity) {
+        Rectangle rayRect = new Rectangle(
+                ray.start.x, ray.start.y, 16, 1
+        );
+        Position pos = cg.getPosition(entity);
+        Size size = cg.getSize(entity);
+        Rectangle entityRect = new Rectangle(pos.x, pos.y, size.width, size.height);
+        return rayRect.overlaps(entityRect);
+    }
+
 }
