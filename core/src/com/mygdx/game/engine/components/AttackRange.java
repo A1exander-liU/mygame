857a01c0188276eb01d17f04ad8d50dec0d031ee
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-11 16:52:20 -0700

    add xRange and yRange the range can extend in either axis

--- core/src/com/mygdx/game/engine/components/AttackRange.java
+++ core/src/com/mygdx/game/engine/components/AttackRange.java
@@ -4,4 +4,10 @@ import com.badlogic.ashley.core.Component;
 
 public class AttackRange implements Component {
     public float range;
+    // will also serve as the length for the attacks
+    // base will be 1 which be adjusted depending on the weapon
+    // player can attack in multiple directions so the range of the attack
+    // will not extend in one axis
+    public float xRange = 1;
+    public float yRange = 1;
 }
