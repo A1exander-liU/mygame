fc8291577030d76711489a9e2d1313bf45e4b5b2
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-12 15:43:50 -0700

    create listener to remove enemy from world

---
+++ core/src/com/mygdx/game/engine/entityListeners/EnemyRemovalListener.java
@@ -0,0 +1,34 @@
+package com.mygdx.game.engine.entityListeners;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.EntityListener;
+import com.mygdx.game.engine.ComponentGrabber;
+import com.mygdx.game.engine.Families;
+import com.mygdx.game.engine.components.Item;
+import com.mygdx.game.engine.systems.CollisionSystem;
+
+public class EnemyRemovalListener implements EntityListener {
+    ComponentGrabber cg;
+
+    public EnemyRemovalListener(ComponentGrabber cg) {
+        this.cg = cg;
+    }
+
+    @Override
+    public void entityAdded(Entity entity) {
+
+    }
+
+    @Override
+    public void entityRemoved(Entity entity) {
+        if (Families.enemies.matches(entity)) {
+            removeFromWorld(entity);
+            System.out.println("enemy removed from world");
+        }
+    }
+
+    private void removeFromWorld(Entity entity) {
+        Item item = cg.getItem(entity);
+        CollisionSystem.world.remove(item.item);
+    }
+}
