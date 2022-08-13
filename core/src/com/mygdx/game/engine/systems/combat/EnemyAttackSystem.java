d29e01ab4e8656d7e439ff8df37cd231ebeaf592
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-13 15:10:55 -0700

    start on EnemyAttackSystem

---
+++ core/src/com/mygdx/game/engine/systems/combat/EnemyAttackSystem.java
@@ -0,0 +1,26 @@
+package com.mygdx.game.engine.systems.combat;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.EntitySystem;
+import com.badlogic.ashley.utils.ImmutableArray;
+import com.mygdx.game.MyGame;
+import com.mygdx.game.engine.ComponentGrabber;
+import com.mygdx.game.engine.Families;
+
+public class EnemyAttackSystem extends EntitySystem {
+    ComponentGrabber cg;
+    ImmutableArray<Entity> enemies;
+
+    public EnemyAttackSystem(ComponentGrabber cg) {
+        super(9);
+        this.cg = cg;
+        enemies = MyGame.engine.getEntitiesFor(Families.enemies);
+    }
+
+    @Override
+    public void update(float delta) {
+        for (int i = 0; i < enemies.size(); i++) {
+            Entity enemy = enemies.get(i);
+        }
+    }
+}
