9cfe8f8eb5fbbecada7a383c8383714b827146ac
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-13 15:39:50 -0700

    check if the enemy can attack again check if the time elpased since their last attack is greater than or equal to their attack speed

--- core/src/com/mygdx/game/engine/systems/combat/EnemyAttackSystem.java
+++ core/src/com/mygdx/game/engine/systems/combat/EnemyAttackSystem.java
@@ -4,20 +4,26 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.ashley.utils.ImmutableArray;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.JsonValue;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.engine.ComponentGrabber;
 import com.mygdx.game.engine.EnemyState;
 import com.mygdx.game.engine.Families;
+import com.mygdx.game.engine.components.Enemy;
+import com.mygdx.game.engine.components.Name;
 import com.mygdx.game.engine.components.ParameterComponent;
+import com.mygdx.game.engine.systems.TimeSystem;
 
 public class EnemyAttackSystem extends EntitySystem {
     ComponentGrabber cg;
+    MyGame root;
     ImmutableArray<Entity> enemies;
     Entity player;
 
-    public EnemyAttackSystem(ComponentGrabber cg) {
+    public EnemyAttackSystem(ComponentGrabber cg, MyGame root) {
         super(9);
         this.cg = cg;
+        this.root = root;
         enemies = MyGame.engine.getEntitiesFor(Families.enemies);
         player = MyGame.engine.getEntitiesFor(Families.player).get(0);
     }
@@ -39,8 +45,20 @@ public class EnemyAttackSystem extends EntitySystem {
     }
 
     private void performAttack(Entity enemy) {
-        ParameterComponent playerParams = cg.getParameters(player);
-        ParameterComponent enemyParams = cg.getParameters(enemy);
-        playerParams.health.currentHealth -= enemyParams.damage;
+        // get the enemy to find their attack speed
+        JsonValue jsonEnemy = root.jsonSearcher.findByEnemyName(cg.getName(enemy).name);
+        float attackSpeed = jsonEnemy.getFloat("ATK_SPD");
+        // if timeOfLastAttack == 0 means they haven't attacked once
+        // and for future attacks it will check if elapsed game time is
+        // greater than or equal to the lastTimeOfAttack by the value of
+        // their attack speed
+        if (cg.getEnemy(enemy).timeOfLastAttack == 0 ||
+        TimeSystem.time - cg.getEnemy(enemy).timeOfLastAttack >= attackSpeed) {
+            // update the timeOfLastAttack
+            cg.getEnemy(enemy).timeOfLastAttack = TimeSystem.time;
+            ParameterComponent playerParams = cg.getParameters(player);
+            ParameterComponent enemyParams = cg.getParameters(enemy);
+            playerParams.health.currentHealth -= enemyParams.damage;
+        }
     }
 }
