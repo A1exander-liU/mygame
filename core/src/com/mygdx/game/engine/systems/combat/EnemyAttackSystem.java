43ce8f04341cdd1a67c05d7944eaf77e475a0128
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-13 15:18:36 -0700

    check if enemy is close to player

--- core/src/com/mygdx/game/engine/systems/combat/EnemyAttackSystem.java
+++ core/src/com/mygdx/game/engine/systems/combat/EnemyAttackSystem.java
@@ -3,6 +3,7 @@ package com.mygdx.game.engine.systems.combat;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.engine.ComponentGrabber;
 import com.mygdx.game.engine.EnemyState;
@@ -11,20 +12,28 @@ import com.mygdx.game.engine.Families;
 public class EnemyAttackSystem extends EntitySystem {
     ComponentGrabber cg;
     ImmutableArray<Entity> enemies;
+    Entity player;
 
     public EnemyAttackSystem(ComponentGrabber cg) {
         super(9);
         this.cg = cg;
         enemies = MyGame.engine.getEntitiesFor(Families.enemies);
+        player = MyGame.engine.getEntitiesFor(Families.player).get(0);
     }
 
     @Override
     public void update(float delta) {
         for (int i = 0; i < enemies.size(); i++) {
             Entity enemy = enemies.get(i);
-            if (cg.getStateMachine(enemy).getCurrentState() == EnemyState.HUNT) {
+            if (cg.getStateMachine(enemy).getCurrentState() == EnemyState.HUNT && closeToPlayer(enemy)) {
 
             }
         }
+    }
+
+    private boolean closeToPlayer(Entity enemy) {
+        Vector2 playerPos = new Vector2(cg.getPosition(player).x, cg.getPosition(player).y);
+        Vector2 enemyPos = new Vector2(cg.getPosition(enemy).x, cg.getPosition(enemy).y);
+        return enemyPos.dst(playerPos) <= 60;
     }
 }
