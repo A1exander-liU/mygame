33976aa7801d36019d760505f74ac023c1f1adee
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-14 13:43:06 -0700

    place level label, place health and exp in nested table

--- core/src/com/mygdx/game/engine/systems/PlayerHudRenderSystem.java
+++ core/src/com/mygdx/game/engine/systems/PlayerHudRenderSystem.java
@@ -5,6 +5,7 @@ import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Cell;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -36,16 +37,22 @@ public class PlayerHudRenderSystem extends EntitySystem {
         root.setSize(playerHud.getWidth(), playerHud.getHeight());
         // add root table to the stage
         playerHud.addActor(root);
-
         // create nested table to display player hp and exp for now
         Table playerHealthAndExp = new Table();
-
+        playerHealthAndExp.setDebug(true);
         // setting the table width and height
         playerHealthAndExp.setSize(root.getWidth() / 3, root.getHeight() / 5);
         // set table background
         playerHealthAndExp.setBackground(skin.getDrawable("button-up"));
         // adding the table to the stage use expand, top, left to move table to top left corner
         root.add(playerHealthAndExp).expand().top().left().height(playerHealthAndExp.getHeight()).width(playerHealthAndExp.getWidth());
+        // create and add level label to the table
+        Label playerLevelLabel = new Label("1", skin);
+        playerHealthAndExp.add(playerLevelLabel).fill();
+
+        // nests a table in this table to have a cell span multiple rows
+        Table playerLevel = new Table();
+        playerHealthAndExp.add(playerLevel);
 
         // create player health bar
         ProgressBar playerHealthBar = new ProgressBar(0, 100, 1, false, skin, "progress-bar-player-health");
@@ -53,9 +60,9 @@ public class PlayerHudRenderSystem extends EntitySystem {
         playerHealthBar.setValue(calcCurrentRemainingHealth());
         playerHealthBar.setName("playerHealthBar");
         // adding to the nested table
-        playerHealthAndExp.add(playerHealthBar);
+        playerLevel.add(playerHealthBar);
         // create new row to add exp bar under health bar
-        playerHealthAndExp.row();
+        playerLevel.row();
 
         // create player exp bar
         ProgressBar playerExpBar = new ProgressBar(0, 100, 1, false, skin, "progress-bar-player-exp");
@@ -63,19 +70,19 @@ public class PlayerHudRenderSystem extends EntitySystem {
         playerExpBar.setValue(10);
         playerExpBar.setName("playerExpBar");
         // add to the nested table
-        playerHealthAndExp.add(playerExpBar);
+        playerLevel.add(playerExpBar);
 
         // grabbing the cells to adjust their size
-        Cell<ProgressBar> healthBarCell = playerHealthAndExp.getCell(playerHealthBar);
+        Cell<ProgressBar> healthBarCell = playerLevel.getCell(playerHealthBar);
         // adjust width and height to leave space for other ui
         // add padding to move the bars towards right edge of the table
-        healthBarCell.height(playerHealthAndExp.getHeight() / 2);
-        healthBarCell.width(playerHealthAndExp.getWidth() * 1.7f / 3);
-        healthBarCell.pad(3, 65, 0, 0);
-        Cell<ProgressBar> expBarCell = playerHealthAndExp.getCell(playerExpBar);
-        expBarCell.height(playerHealthAndExp.getHeight() / 2);
-        expBarCell.width(playerHealthAndExp.getWidth() * 1.7f / 3);
-        expBarCell.pad(0, 65, 0, 0);
+//        healthBarCell.height(playerLevel.getHeight() / 2);
+//        healthBarCell.width(playerLevel.getWidth() * 1.7f / 3);
+        healthBarCell.pad(3, 50, 0, 0);
+        Cell<ProgressBar> expBarCell = playerLevel.getCell(playerExpBar);
+//        expBarCell.height(playerLevel.getHeight() / 2);
+//        expBarCell.width(playerLevel.getWidth() * 1.7f / 3);
+        expBarCell.pad(0, 50, 0, 0);
 
         playerHud.act();
         playerHud.draw();
