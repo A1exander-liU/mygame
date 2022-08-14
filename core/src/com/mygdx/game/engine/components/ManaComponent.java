c5a836ab4e34e35494ef42d5e12c1fc8045840f0
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-14 14:45:53 -0700

    add mana component

---
+++ core/src/com/mygdx/game/engine/components/ManaComponent.java
@@ -0,0 +1,8 @@
+package com.mygdx.game.engine.components;
+
+import com.badlogic.ashley.core.Component;
+
+public class ManaComponent implements Component {
+    public float maxMana = 10;
+    public float currentMana = 10;
+}
