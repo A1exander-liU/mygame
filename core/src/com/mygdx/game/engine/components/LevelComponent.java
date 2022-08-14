c5ccebb7053e38a3be623cebccd92ec4f7d0d8be
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-14 15:08:52 -0700

    change value type to int dont want decimal

--- core/src/com/mygdx/game/engine/components/LevelComponent.java
+++ core/src/com/mygdx/game/engine/components/LevelComponent.java
@@ -3,5 +3,5 @@ package com.mygdx.game.engine.components;
 import com.badlogic.ashley.core.Component;
 
 public class LevelComponent implements Component {
-    public float level = 1;
+    public int level = 1;
 }
