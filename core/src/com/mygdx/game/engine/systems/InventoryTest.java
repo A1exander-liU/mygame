4135561568f8bc54449c659f0133f55c41108ad0
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-15 16:38:52 -0700

    create toggle inventory

--- core/src/com/mygdx/game/engine/systems/InventoryTest.java
+++ core/src/com/mygdx/game/engine/systems/InventoryTest.java
@@ -2,15 +2,19 @@ package com.mygdx.game.engine.systems;
 
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.engine.ComponentGrabber;
 import com.mygdx.game.engine.Families;
+import com.mygdx.game.engine.components.InventoryComponent;
 
 public class InventoryTest extends EntitySystem {
     ComponentGrabber cg;
     Entity player;
 
     public InventoryTest(ComponentGrabber cg) {
+        super(101);
         this.cg = cg;
         player = MyGame.engine.getEntitiesFor(Families.player).get(0);
 
@@ -18,6 +22,18 @@ public class InventoryTest extends EntitySystem {
 
     @Override
     public void update(float delta) {
+        toggleInventory();
+    }
 
+    private void toggleInventory() {
+        InventoryComponent inventory = cg.getInventory(player);
+        if (!inventory.opened && Gdx.input.isKeyJustPressed(Input.Keys.B)) {
+            inventory.opened = true;
+            System.out.println("opened inventory");
+        }
+        else if (inventory.opened && Gdx.input.isKeyJustPressed(Input.Keys.B)) {
+            inventory.opened = false;
+            System.out.println("closed inventory");
+        }
     }
 }
