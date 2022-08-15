1eba4c07e1ee1d4e95fd70b27832fbf4d0536671
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-14 17:38:37 -0700

    add exp component

---
+++ core/src/com/mygdx/game/engine/components/ExpComponent.java
@@ -0,0 +1,8 @@
+package com.mygdx.game.engine.components;
+
+import com.badlogic.ashley.core.Component;
+
+public class ExpComponent implements Component {
+    public int currentExp = 0;
+    public int toNextLevel = 100;
+}
