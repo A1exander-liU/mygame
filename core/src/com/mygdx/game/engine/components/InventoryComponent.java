b1f57d19647dcb7e875ee1a4d01af3a9fd89494c
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-15 16:35:01 -0700

    add opened field

--- core/src/com/mygdx/game/engine/components/InventoryComponent.java
+++ core/src/com/mygdx/game/engine/components/InventoryComponent.java
@@ -8,4 +8,23 @@ public class InventoryComponent implements Component {
     // how many unique items can be held
     public int capacity = 20;
     public Array<Entity> items = new Array<>(capacity);
+    public boolean opened = false;
+    // each item will contain these components:
+    // ALL: all items must have, ONE: have one of the listed
+    // ALL:
+    // InventoryItemComponent:
+    // NameComponent:
+    // RarityComponent:
+    // DescriptionComponent:
+
+    // ONE:
+    // EquipmentComponent: Will also have enum to determine the type
+    // ConsumableComponent:
+    // MaterialComponent:
+
+    // StackableComponent: a flag to determine item is stackable
+    // QuantityComponent: for items where multiple can be obtained at once
+    // like getting 3 wood for chopping a tree
+
+
 }
