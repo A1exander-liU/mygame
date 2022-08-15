2808418e3709d9504a3b9cf8f2860c8384dd489d
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-15 16:20:34 -0700

    add capacity field

--- core/src/com/mygdx/game/engine/components/InventoryComponent.java
+++ core/src/com/mygdx/game/engine/components/InventoryComponent.java
@@ -5,5 +5,7 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.utils.Array;
 
 public class InventoryComponent implements Component {
-    public Array<Entity> items = new Array<>();
+    // how many unique items can be held
+    public int capacity = 20;
+    public Array<Entity> items = new Array<>(capacity);
 }
