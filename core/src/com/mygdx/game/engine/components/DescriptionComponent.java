966b028acf2a3246f5cfc1d5f77d5b3f4919981e
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-16 15:39:44 -0700

    add description component

---
+++ core/src/com/mygdx/game/engine/components/DescriptionComponent.java
@@ -0,0 +1,7 @@
+package com.mygdx.game.engine.components;
+
+import com.badlogic.ashley.core.Component;
+
+public class DescriptionComponent implements Component {
+    public String description;
+}
