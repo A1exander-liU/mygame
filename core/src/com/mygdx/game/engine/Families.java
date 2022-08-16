2c09f3a0320a2862fe8926ce23f7870fda1c5650
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-16 13:54:32 -0700

    change obstacles and collisions family use obstacle and collidable flag components instead

--- core/src/com/mygdx/game/engine/Families.java
+++ core/src/com/mygdx/game/engine/Families.java
@@ -1,8 +1,10 @@
 package com.mygdx.game.engine;
 
 import com.badlogic.ashley.core.Family;
+import com.mygdx.game.engine.components.CollidableComponent;
 import com.mygdx.game.engine.components.Enemy;
 import com.mygdx.game.engine.components.InventoryItemComponent;
+import com.mygdx.game.engine.components.ObstacleComponent;
 import com.mygdx.game.engine.components.Player;
 import com.mygdx.game.engine.components.SpawnArea;
 
@@ -10,9 +12,9 @@ public class Families {
 
     public static final Family enemies = Family.all(Enemy.class).get();
     public static final Family player = Family.all(Player.class).get();
-    public static final Family obstacles = Family.exclude(Enemy.class, Player.class, SpawnArea.class).get();
+    public static final Family obstacles = Family.all(ObstacleComponent.class).get();
     public static final Family spawns = Family.all(SpawnArea.class).get();
-    public static final Family collisions = Family.exclude(SpawnArea.class).get();
+    public static final Family collisions = Family.all(CollidableComponent.class).get();
     public static final Family characters = Family.one(Player.class, Enemy.class).get();
     public static final Family items = Family.all(InventoryItemComponent.class).get();
 
