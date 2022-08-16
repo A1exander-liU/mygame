5112603547b41c78a19759aa7000d39580f9de74
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-16 14:07:47 -0700

    print out inventory

--- core/src/com/mygdx/game/engine/systems/InventoryTest.java
+++ core/src/com/mygdx/game/engine/systems/InventoryTest.java
@@ -8,6 +8,7 @@ import com.mygdx.game.MyGame;
 import com.mygdx.game.engine.ComponentGrabber;
 import com.mygdx.game.engine.Families;
 import com.mygdx.game.engine.components.InventoryComponent;
+import com.mygdx.game.engine.components.InventorySlotComponent;
 
 public class InventoryTest extends EntitySystem {
     ComponentGrabber cg;
@@ -22,18 +23,31 @@ public class InventoryTest extends EntitySystem {
 
     @Override
     public void update(float delta) {
-        toggleInventory();
+        InventoryComponent inventory = cg.getInventory(player);
+        toggleInventory(inventory);
     }
 
-    private void toggleInventory() {
-        InventoryComponent inventory = cg.getInventory(player);
+    private void toggleInventory(InventoryComponent inventory) {
         if (!inventory.opened && Gdx.input.isKeyJustPressed(Input.Keys.B)) {
             inventory.opened = true;
             System.out.println("opened inventory");
+            getInventory(inventory);
         }
         else if (inventory.opened && Gdx.input.isKeyJustPressed(Input.Keys.B)) {
             inventory.opened = false;
             System.out.println("closed inventory");
+        }
+    }
+
+    private void getInventory(InventoryComponent inventory) {
+        for (int i = 0; i < inventory.items.size; i++) {
+            Entity slot = inventory.items.get(i);
+            InventorySlotComponent slotComponent = cg.getInventorySlot(slot);
+            System.out.println("----------");
+            System.out.println("Slot " + (i + 1));
+            System.out.println((slotComponent.itemOccupied == null) ? "empty": slotComponent.itemOccupied);
+            System.out.println("Amount: " + slotComponent.quantity);
+            System.out.println("----------");
         }
     }
 }
