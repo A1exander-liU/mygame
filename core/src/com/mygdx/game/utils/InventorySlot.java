e0240280bd3865097e7c336bce8be7d77b61ce12
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-18 14:18:47 -0700

    start on InventorySlot add extra functionality to inventory by keeping a reference of the item and for swapping and stacking items

---
+++ core/src/com/mygdx/game/utils/InventorySlot.java
@@ -0,0 +1,24 @@
+package com.mygdx.game.utils;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.mygdx.game.MyGame;
+import com.mygdx.game.engine.Mappers;
+
+import java.util.Objects;
+
+public class InventorySlot extends Button {
+    // the item in this slot
+    // this will be the full info
+    Entity occupiedItem;
+
+    public InventorySlot(Skin skin) {
+        super(skin);
+    }
+
+    public Entity getOccupiedItem() {
+        return this.occupiedItem;
+    }
+
+}
