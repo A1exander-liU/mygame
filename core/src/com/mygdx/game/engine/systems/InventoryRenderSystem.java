949ee9de5d746f80010546ae980709fa2c108920
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-18 15:52:32 -0700

    create payload

--- core/src/com/mygdx/game/engine/systems/InventoryRenderSystem.java
+++ core/src/com/mygdx/game/engine/systems/InventoryRenderSystem.java
@@ -252,9 +252,14 @@ public class InventoryRenderSystem extends EntitySystem {
         dragAndDrop.addSource(new DragAndDrop.Source(itemStack) {
             @Override
             public DragAndDrop.Payload dragStart(InputEvent event, float x, float y, int pointer) {
-                return null;
+                DragAndDrop.Payload payload = new DragAndDrop.Payload();
+                // the actor that will be dragged
+                payload.setDragActor(getActor());
+
+                return payload;
             }
         });
+
 
         inventorySlots.add(slot).width(inventorySlots.getWidth() / 4).height(inventorySlots.getHeight() / 4);
     }
