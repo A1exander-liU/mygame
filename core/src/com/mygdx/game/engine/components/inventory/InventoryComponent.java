8d91a7ed3215e347dff0b86f9f7cf4232b472870
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-18 18:11:29 -0700

    create array of inventory slots add source and target for drag and drop

--- core/src/com/mygdx/game/engine/components/inventory/InventoryComponent.java
+++ core/src/com/mygdx/game/engine/components/inventory/InventoryComponent.java
@@ -2,14 +2,20 @@ package com.mygdx.game.engine.components.inventory;
 
 import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
 import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.MyGame;
+import com.mygdx.game.engine.InventorySlotSource;
+import com.mygdx.game.engine.InventorySlotTarget;
+import com.mygdx.game.utils.InventorySlot;
 
 public class InventoryComponent implements Component {
     // how many unique items can be held
     public int capacity = 16;
     public Array<Entity> items = new Array<>(capacity);
+    public Array<InventorySlot> inventorySlots = new Array<>(capacity);
     public boolean opened = false;
+    public DragAndDrop dragAndDrop = new DragAndDrop();
     // each item will contain these components:
     // ALL: all items must have, ONE: have one of the listed
     // ALL:
@@ -34,6 +40,11 @@ public class InventoryComponent implements Component {
             slot.add(new InventorySlotComponent());
             items.add(slot);
             MyGame.engine.addEntity(slot);
+        }
+        for (int i = 0; i < inventorySlots.size; i++) {
+            InventorySlot inventorySlot = inventorySlots.get(i);
+            dragAndDrop.addSource(new InventorySlotSource(inventorySlot, dragAndDrop));
+            dragAndDrop.addTarget(new InventorySlotTarget(inventorySlot));
         }
     }
 
