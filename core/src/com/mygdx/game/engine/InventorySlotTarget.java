ba1b1c8f99bda25864ae2ed9e71c956ee6304491
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-18 17:41:42 -0700

    start on InventorySlotTarget

---
+++ core/src/com/mygdx/game/engine/InventorySlotTarget.java
@@ -0,0 +1,26 @@
+package com.mygdx.game.engine;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
+import com.mygdx.game.utils.InventorySlot;
+
+public class InventorySlotTarget extends DragAndDrop.Target {
+
+    Entity targetItem;
+
+    public InventorySlotTarget(InventorySlot actor) {
+        super(actor);
+        targetItem = actor.getOccupiedItem();
+    }
+
+    @Override
+    public boolean drag(DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
+        return false;
+    }
+
+    @Override
+    public void drop(DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
+
+    }
+}
