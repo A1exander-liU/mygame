0244aad695c1039fc07d4de2f5f067121941e7c7
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-20 14:28:36 -0700

    fill constructor

--- core/src/com/mygdx/game/JsonItemFinder.java
+++ core/src/com/mygdx/game/JsonItemFinder.java
@@ -1,2 +1,15 @@
-package com.mygdx.game;public class JsonItemFinder {
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
+
+public class JsonItemFinder extends JsonReader {
+    private final JsonValue materials;
+
+    public JsonItemFinder() {
+        super();
+        materials = super.parse(Gdx.files.internal("gameData/itemData/materials.json")).get("materials");
+    }
+    
 }
