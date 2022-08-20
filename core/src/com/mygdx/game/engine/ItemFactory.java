d12d44f5092a54cf38060138995388ba14f00081
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-20 14:38:59 -0700

    add and modify material components

--- core/src/com/mygdx/game/engine/ItemFactory.java
+++ core/src/com/mygdx/game/engine/ItemFactory.java
@@ -1,12 +1,35 @@
 package com.mygdx.game.engine;
 
 
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.JsonValue;
 import com.mygdx.game.JsonItemFinder;
+import com.mygdx.game.engine.components.Name;
+import com.mygdx.game.engine.components.Sprite;
+import com.mygdx.game.engine.components.inventory.items.shared.DescriptionComponent;
+import com.mygdx.game.engine.components.inventory.items.shared.QuantityComponent;
+import com.mygdx.game.engine.components.inventory.items.shared.RarityComponent;
 
 public class ItemFactory {
     JsonItemFinder itemFinder;
 
     public ItemFactory(JsonItemFinder itemFinder) {
         this.itemFinder = itemFinder;
+    }
+
+    public void makeMaterial(String materialName, int amount) {
+        JsonValue material = itemFinder.findMaterialByName(materialName);
+        Entity materialEntity = new Entity();
+        materialEntity.add(new Name());
+        materialEntity.add(new Sprite());
+        materialEntity.add(new RarityComponent());
+        materialEntity.add(new DescriptionComponent());
+        materialEntity.add(new QuantityComponent());
+        Mappers.name.get(materialEntity).name = material.getString("name");
+        Mappers.sprite.get(materialEntity).texture = new Texture(Gdx.files.internal(material.getString("sprite")));
+        
+        Mappers.description.get(materialEntity).description = material.getString("desc");
     }
 }
