a3a7912ca4ae7d9c2943074eac7c23a3f6244c78
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-20 14:33:14 -0700

    start on ItemFactory

---
+++ core/src/com/mygdx/game/engine/ItemFactory.java
@@ -0,0 +1,12 @@
+package com.mygdx.game.engine;
+
+
+import com.mygdx.game.JsonItemFinder;
+
+public class ItemFactory {
+    JsonItemFinder itemFinder;
+
+    public ItemFactory(JsonItemFinder itemFinder) {
+        this.itemFinder = itemFinder;
+    }
+}
