9468e4c29852bca23cae8e2c7ece62266567920f
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-20 14:31:09 -0700

    add method find material

--- core/src/com/mygdx/game/JsonItemFinder.java
+++ core/src/com/mygdx/game/JsonItemFinder.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
 
+import java.util.Objects;
+
 public class JsonItemFinder extends JsonReader {
     private final JsonValue materials;
 
@@ -11,5 +13,13 @@ public class JsonItemFinder extends JsonReader {
         super();
         materials = super.parse(Gdx.files.internal("gameData/itemData/materials.json")).get("materials");
     }
-    
+
+    public JsonValue findMaterialByName(String itemName) {
+        for (JsonValue material : materials) {
+            if (Objects.equals(material.getString("name"), itemName))
+                return material;
+        }
+        return null;
+    }
+
 }
