bed6a6a93ce8a1aa2615a65de50f292d4bc5f55c
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-21 14:43:39 -0700

    create skin

--- core/src/com/mygdx/game/InventoryChangeListener.java
+++ core/src/com/mygdx/game/InventoryChangeListener.java
@@ -1,25 +1,46 @@
 package com.mygdx.game;
 
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.mygdx.game.engine.ItemType;
+import com.mygdx.game.engine.Mappers;
 import com.mygdx.game.utils.InventorySlot;
 
 public class InventoryChangeListener extends ChangeListener {
 
     Stage stage;
+    Skin skin;
 
     public InventoryChangeListener() {
         stage = new Stage(new ScreenViewport());
+
+        skin = new Skin();
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/PressStart2P-Regular.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 9;
+        BitmapFont newFont = generator.generateFont(parameter);
+        skin.add("pixel2D", newFont);
+        skin.addRegions(new TextureAtlas("Game_UI_Skin/Game_UI_Skin.atlas"));
+        skin.load(Gdx.files.internal("Game_UI_Skin/Game_UI_Skin.json"));
+        generator.dispose();
     }
 
     @Override
     public void changed(ChangeEvent event, Actor actor) {
         InventorySlot inventorySlot = (InventorySlot) actor;
-        // check if inventory slot holds an item
-        if (!inventorySlot.isEmpty()) {
-            System.out.println("has an item");
+        Entity occupiedItem = inventorySlot.getOccupiedItem();
+        // check if inventory slot holds an item and is a material type
+        if (!inventorySlot.isEmpty() && Mappers.inventoryItem.get(occupiedItem).itemType == ItemType.MATERIAL) {
+    
         }
     }
 }
