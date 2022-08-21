a2ddba58e104bc0b20aea4f5dc23ae0f5c8ef300
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-21 14:33:06 -0700

    add ItemType field

--- core/src/com/mygdx/game/engine/components/inventory/items/shared/InventoryItemComponent.java
+++ core/src/com/mygdx/game/engine/components/inventory/items/shared/InventoryItemComponent.java
@@ -1,6 +1,14 @@
 package com.mygdx.game.engine.components.inventory.items.shared;
 
 import com.badlogic.ashley.core.Component;
+import com.mygdx.game.engine.ItemType;
 
 public class InventoryItemComponent implements Component {
+    public ItemType itemType;
+
+    public InventoryItemComponent() {}
+
+    public InventoryItemComponent(ItemType itemType) {
+        this.itemType = itemType;
+    }
 }
