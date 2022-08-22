ae6f2a3b46ee6cf46c2d9165fc7e588f9ad9826a
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-21 17:32:28 -0700

    make and show dialog

--- core/src/com/mygdx/game/engine/systems/ItemWindowRenderSystem.java
+++ core/src/com/mygdx/game/engine/systems/ItemWindowRenderSystem.java
@@ -2,8 +2,16 @@ package com.mygdx.game.engine.systems;
 
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.engine.Families;
@@ -17,12 +25,24 @@ public class ItemWindowRenderSystem extends EntitySystem {
     Entity player;
     Array<InventorySlot> inventorySlots;
     Array<InventorySlot> equipSlots;
+    Skin skin;
 
     public ItemWindowRenderSystem() {
+
         player = MyGame.engine.getEntitiesFor(Families.player).get(0);
         InventoryComponent inventoryComponent = Mappers.inventory.get(player);
         inventorySlots = inventoryComponent.inventorySlots;
         equipSlots = inventoryComponent.equipSlots;
+
+        skin = new Skin();
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/PressStart2P-Regular.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 9;
+        BitmapFont newFont = generator.generateFont(parameter);
+        skin.add("pixel2D", newFont);
+        skin.addRegions(new TextureAtlas("Game_UI_Skin/Game_UI_Skin.atlas"));
+        skin.load(Gdx.files.internal("Game_UI_Skin/Game_UI_Skin.json"));
+        generator.dispose();
     }
 
     @Override
@@ -32,7 +52,8 @@ public class ItemWindowRenderSystem extends EntitySystem {
                 inventorySlots.get(i).setClicked(false);
                 // add button to call dialog.hide to remove it
                 Stage stage = inventorySlots.get(i).getStage();
-                
+                Dialog itemWindow = makeItemWindow(inventorySlots.get(i).getOccupiedItem());
+                itemWindow.show(stage);
             }
         }
         for (int i = 0; i < equipSlots.size; i++) {
@@ -40,5 +61,15 @@ public class ItemWindowRenderSystem extends EntitySystem {
                 equipSlots.get(i).setClicked(false);
             }
         }
+    }
+
+    private Dialog makeItemWindow(Entity occupiedItem) {
+        Dialog dialog = new Dialog("" + Mappers.name.get(occupiedItem).name, skin);
+        dialog.setPosition(0, 0);
+        Label desc = new Label(Mappers.description.get(occupiedItem).description, skin, "pixel2D", Color.BLACK);
+        desc.setAlignment(Align.topLeft);
+        desc.setWrap(true);
+        dialog.text(desc);
+        return dialog;
     }
 }
