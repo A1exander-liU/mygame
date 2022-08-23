df31277a7883a966255a114e78233db4a80b8f5b
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-23 15:39:26 -0700

    close when certain button is clicked

--- core/src/com/mygdx/game/ItemInfoDialog.java
+++ core/src/com/mygdx/game/ItemInfoDialog.java
@@ -1,9 +1,15 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.Align;
+import com.mygdx.game.utils.InventorySlot;
 
 public class ItemInfoDialog extends Dialog {
+
     public ItemInfoDialog(String title, Skin skin) {
         super(title, skin);
     }
@@ -18,13 +24,18 @@ public class ItemInfoDialog extends Dialog {
     }
 
     {
-        text("Close?");
-        button("Yes", true);
-        button("No", false);
+        Label label = new Label("Close?", getSkin(), "pixel2D", Color.BLACK);
+        text(label);
     }
 
     @Override
     protected void result(Object object) {
+        if (object.equals("yes")) {
+            hide();
+        }
+        else {
+            cancel();
+        }
 
     }
 
