295114d8c2089cf29fc9e75ab43c394863523ce7
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-24 16:28:58 -0700

    make ArmourStatComponent

---
+++ core/src/com/mygdx/game/engine/components/inventory/items/individual/ArmourStatComponent.java
@@ -0,0 +1,10 @@
+package com.mygdx.game.engine.components.inventory.items.individual;
+
+import com.badlogic.ashley.core.Component;
+
+public class ArmourStatComponent implements Component {
+    public int physicalDef;
+    public int magicalDef;
+    public int hp;
+    public int mp;
+}
