4078de49ae83313623152710975492d4fb5a2178
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-24 14:20:32 -0700

    add stackSize field

--- core/src/com/mygdx/game/engine/components/inventory/items/individual/StackableComponent.java
+++ core/src/com/mygdx/game/engine/components/inventory/items/individual/StackableComponent.java
@@ -3,4 +3,5 @@ package com.mygdx.game.engine.components.inventory.items.individual;
 import com.badlogic.ashley.core.Component;
 
 public class StackableComponent implements Component {
+    public int stackSize = 99; // default stack size
 }
