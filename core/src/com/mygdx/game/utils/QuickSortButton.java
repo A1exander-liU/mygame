462f857c647f356e904273549733e93b1f81f4e5
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-27 15:14:42 -0700

    remove enums and hashmap

--- core/src/com/mygdx/game/utils/QuickSortButton.java
+++ core/src/com/mygdx/game/utils/QuickSortButton.java
@@ -16,53 +16,9 @@ import java.util.HashMap;
 public class QuickSortButton extends ImageButton {
     Entity player;
 
-    HashMap<Rarity, RarityValue> rarities;
-
-    enum RarityValue {
-        COMMON    (1),
-        UNCOMMON  (2),
-        RARE      (3),
-        EPIC      (4),
-        LEGENDARY (5),
-        MYTHICAL  (6);
-
-        int value;
-
-        public int getValue() {
-            return value;
-        }
-
-        RarityValue(int value) {
-            this.value = value;
-        }
-    }
-
-    enum Type {
-        MAIN       (1),
-        OFF        (2),
-        ACCESSORY  (3),
-        HEAD       (4),
-        TORSO      (5),
-        LEG        (6),
-        FEET       (7),
-        CONSUMABLE (8),
-        MATERIAL   (9);
-
-        int value;
-
-        public int getValue() {
-            return value;
-        }
-
-        Type(int value) {
-            this.value = value;
-        }
-    }
-
     public QuickSortButton(Skin skin) {
         super(skin);
         player = MyGame.engine.getEntitiesFor(Families.player).get(0);
-        makeRarities();
         addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
@@ -89,13 +45,5 @@ public class QuickSortButton extends ImageButton {
             }
         }
     }
-
-    private void makeRarities() {
-        rarities.put(Rarity.COMMON, RarityValue.COMMON);
-        rarities.put(Rarity.UNCOMMON, RarityValue.UNCOMMON);
-        rarities.put(Rarity.RARE, RarityValue.RARE);
-        rarities.put(Rarity.EPIC, RarityValue.EPIC);
-        rarities.put(Rarity.LEGENDARY, RarityValue.LEGENDARY);
-        rarities.put(Rarity.MYTHICAL, RarityValue.MYTHICAL);
-    }
+    
 }
