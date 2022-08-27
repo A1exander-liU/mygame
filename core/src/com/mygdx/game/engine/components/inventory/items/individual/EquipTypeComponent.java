8e386c942534ca5c74271f2d98a3ce306ed86c04
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-27 14:22:34 -0700

    add Type field and constructor with Type param

--- core/src/com/mygdx/game/engine/components/inventory/items/individual/EquipTypeComponent.java
+++ core/src/com/mygdx/game/engine/components/inventory/items/individual/EquipTypeComponent.java
@@ -2,13 +2,19 @@ package com.mygdx.game.engine.components.inventory.items.individual;
 
 import com.badlogic.ashley.core.Component;
 import com.mygdx.game.engine.AcceptedEquipType;
+import com.mygdx.game.engine.Type;
 
 public class EquipTypeComponent implements Component {
     public AcceptedEquipType acceptedEquipType;
+    public Type acceptedType;
 
     public EquipTypeComponent() {}
 
     public EquipTypeComponent(AcceptedEquipType acceptedEquipType) {
         this.acceptedEquipType = acceptedEquipType;
+    }
+
+    public EquipTypeComponent(Type acceptedType) {
+        this.acceptedType = acceptedType;
     }
 }
