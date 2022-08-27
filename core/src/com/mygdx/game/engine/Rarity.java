0d62d33a9d2434a46ca7f7ea3d0fa8ef8c049bc8
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-27 15:13:35 -0700

    add rank field for sorting later by rarity

--- core/src/com/mygdx/game/engine/Rarity.java
+++ core/src/com/mygdx/game/engine/Rarity.java
@@ -1,5 +1,21 @@
 package com.mygdx.game.engine;
 
 public enum Rarity {
-    COMMON, UNCOMMON, RARE, EPIC, LEGENDARY, MYTHICAL
+    COMMON    (1),
+    UNCOMMON  (2),
+    RARE      (3),
+    EPIC      (4),
+    LEGENDARY (5) ,
+    MYTHICAL  (6);
+
+    int rank;
+
+    public int getRank() {
+        return rank;
+    }
+
+    Rarity(int rank) {
+        this.rank = rank;
+    }
+
 }
