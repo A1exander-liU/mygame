27a1a2ea7067b66d068eb043d7be2f3ade1c3ac9
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-26 17:47:30 -0700

    make rarityEnum

--- core/src/com/mygdx/game/utils/QuickSortButton.java
+++ core/src/com/mygdx/game/utils/QuickSortButton.java
@@ -5,13 +5,35 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.engine.Families;
+import com.mygdx.game.engine.Mappers;
+import com.mygdx.game.engine.Rarity;
+
+import java.util.Dictionary;
 
 public class QuickSortButton extends ImageButton {
     Entity player;
+
+    enum RarityValue {
+        COMMON    (1),
+        UNCOMMON  (2),
+        RARE      (3),
+        EPIC      (4),
+        LEGENDARY (5),
+        MYTHICAL  (6);
+
+        int value;
+
+        public int getValue() {
+            return value;
+        }
+
+        RarityValue(int value) {
+            this.value = value;
+        }
+    }
 
     public QuickSortButton(Skin skin) {
         super(skin);
@@ -20,7 +42,12 @@ public class QuickSortButton extends ImageButton {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 System.out.println("clicked");
+                quickSortInventory();
             }
         });
+
+    }
+
+    private void quickSortInventory() {
     }
 }
