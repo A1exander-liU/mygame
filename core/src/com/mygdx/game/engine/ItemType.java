7b75ff2386347b69d1d40a1b9b1c8b86357f105a
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-27 15:43:40 -0700

    change ItemType ranks

--- core/src/com/mygdx/game/engine/ItemType.java
+++ core/src/com/mygdx/game/engine/ItemType.java
@@ -1,15 +1,14 @@
 package com.mygdx.game.engine;
 
 public enum ItemType {
-    MAIN       (1),
-    OFF        (2),
-    ACCESSORY  (3),
-    HEAD       (4),
+    MATERIAL   (1),
+    CONSUMABLE (2),
+    FEET       (3),
+    LEG        (4),
     TORSO      (5),
-    LEG        (6),
-    FEET       (7),
-    CONSUMABLE (8),
-    MATERIAL   (9);
+    HEAD       (6),
+    OFF        (8),
+    MAIN       (9);
 
     int rank;
 
