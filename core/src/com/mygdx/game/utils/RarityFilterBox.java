908ba43dd2be51acd9ba344ff62d4201afc71142
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-27 18:14:02 -0700

    add listener and method to determine rarity to filter

--- core/src/com/mygdx/game/utils/RarityFilterBox.java
+++ core/src/com/mygdx/game/utils/RarityFilterBox.java
@@ -1,16 +1,53 @@
 package com.mygdx.game.utils;
 
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.Array;
+import com.mygdx.game.engine.Rarity;
 
 public class RarityFilterBox extends SelectBox<String> {
+
+    Rarity currentFilter;
 
     public RarityFilterBox(Skin skin) {
         super(skin);
         setItems("All", "Common", "Uncommon", "Rare", "Epic", "Legendary", "Mythical");
         getList().setAlignment(Align.left);
-
+        addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                determineRarityFilter(getSelected());
+            }
+        });
     }
+
+    private void determineRarityFilter(String selectedFilter) {
+        switch (selectedFilter) {
+            case "All":
+                // null will mean all
+                currentFilter = null;
+                break;
+            case "Common":
+                currentFilter = Rarity.COMMON;
+                break;
+            case "Uncommon":
+                currentFilter = Rarity.UNCOMMON;
+                break;
+            case "Rare":
+                currentFilter = Rarity.RARE;
+                break;
+            case "Epic":
+                currentFilter = Rarity.EPIC;
+                break;
+            case "Legendary":
+                currentFilter = Rarity.LEGENDARY;
+                break;
+            case "Mythical":
+                currentFilter = Rarity.MYTHICAL;
+                break;
+        }
+    }
+
 }
