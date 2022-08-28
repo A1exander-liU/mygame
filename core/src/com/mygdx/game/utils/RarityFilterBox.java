2a9baa729ecf5435e3f39504754eb6dcb2f3a846
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-27 18:17:28 -0700

    start on filter method

--- core/src/com/mygdx/game/utils/RarityFilterBox.java
+++ core/src/com/mygdx/game/utils/RarityFilterBox.java
@@ -1,24 +1,33 @@
 package com.mygdx.game.utils;
 
+import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Array;
+import com.mygdx.game.MyGame;
+import com.mygdx.game.engine.Families;
+import com.mygdx.game.engine.Mappers;
 import com.mygdx.game.engine.Rarity;
 
 public class RarityFilterBox extends SelectBox<String> {
 
     Rarity currentFilter;
+    Entity player;
+    Array<InventorySlot> currentInventory;
 
     public RarityFilterBox(Skin skin) {
         super(skin);
+        player = MyGame.engine.getEntitiesFor(Families.player).get(0);
         setItems("All", "Common", "Uncommon", "Rare", "Epic", "Legendary", "Mythical");
         getList().setAlignment(Align.left);
         addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 determineRarityFilter(getSelected());
+                filter();
             }
         });
     }
@@ -48,6 +57,11 @@ public class RarityFilterBox extends SelectBox<String> {
                 currentFilter = Rarity.MYTHICAL;
                 break;
         }
+    }
+
+    private void filter() {
+        currentInventory = Mappers.inventory.get(player).inventorySlots;
+        Array<InventorySlot> inventorySlots = Mappers.inventory.get(player).inventorySlots;
     }
 
 }
