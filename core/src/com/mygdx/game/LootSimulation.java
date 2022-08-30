26507da3d9b5c44f82d083d5e495cc352b6b8fd4
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-30 15:36:01 -0700

    get loot table of slime instead of looping through all drop tables just get slime drop table

--- core/src/com/mygdx/game/LootSimulation.java
+++ core/src/com/mygdx/game/LootSimulation.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class LootSimulation {
     JsonValue drops;
@@ -19,20 +20,22 @@ public class LootSimulation {
         float totalWeight = 0;
         float[] weights = {};
 
-        for (int i = 0; i < drops.size; i++) {
-            // the object that holds name, always and chance
-            JsonValue lootTable = drops.get(i);
-            // the object array that holds items that have a chance to drop
-            JsonValue chance = lootTable.get("chance");
-            // loop through chance item drops and add up the weights
-            for (JsonValue chanceDrop: chance) {
-                totalWeight += chanceDrop.getFloat("weight");
-                // array size is fixed need to make copy and increase size by one each
-                // time to add new element
-                weights = Arrays.copyOf(weights, weights.length + 1);
-                // copied array is same array with one new element at end
-                weights[weights.length - 1] = chanceDrop.getFloat("weight");
-            }
+        JsonValue lootTable = null;
+
+        for (JsonValue drop: drops) {
+            if (Objects.equals(drop.getString("name"), "slime"))
+                lootTable = drop;
+        }
+        // the object array that holds items that have a chance to drop
+        JsonValue chance = lootTable.get("chance");
+        // loop through chance item drops and add up the weights
+        for (JsonValue chanceDrop: chance) {
+            totalWeight += chanceDrop.getFloat("weight");
+            // array size is fixed need to make copy and increase size by one each
+            // time to add new element
+            weights = Arrays.copyOf(weights, weights.length + 1);
+            // copied array is same array with one new element at end
+            weights[weights.length - 1] = chanceDrop.getFloat("weight");
         }
     }
 
