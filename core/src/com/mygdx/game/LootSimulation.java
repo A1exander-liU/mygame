f25e689b03e94331616e174534da67f8297e5f25
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-30 15:27:41 -0700

    add each weight to weights array

--- core/src/com/mygdx/game/LootSimulation.java
+++ core/src/com/mygdx/game/LootSimulation.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
 
+import java.util.Arrays;
+
 public class LootSimulation {
     JsonValue drops;
 
@@ -15,6 +17,7 @@ public class LootSimulation {
 
     public void fightSlimes(int fights) {
         float totalWeight = 0;
+        float[] weights = {};
 
         for (int i = 0; i < drops.size; i++) {
             // the object that holds name, always and chance
@@ -22,8 +25,14 @@ public class LootSimulation {
             // the object array that holds items that have a chance to drop
             JsonValue chance = lootTable.get("chance");
             // loop through chance item drops and add up the weights
-            for (JsonValue chanceDrop: chance)
+            for (JsonValue chanceDrop: chance) {
                 totalWeight += chanceDrop.getFloat("weight");
+                // array size is fixed need to make copy and increase size by one each
+                // time to add new element
+                weights = Arrays.copyOf(weights, weights.length + 1);
+                // copied array is same array with one new element at end
+                weights[weights.length - 1] = chanceDrop.getFloat("weight");
+            }
         }
     }
 
