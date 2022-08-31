c0a88b1ba2ed8bc7c10681b6dfc161ec9eaa5b3d
AlexanderLiu <alexanderliu2016@gmail.com>
2022-08-31 14:44:50 -0700

    get name of dead enemy

--- core/src/com/mygdx/game/engine/systems/EnemyDropSystem.java
+++ core/src/com/mygdx/game/engine/systems/EnemyDropSystem.java
@@ -5,6 +5,7 @@ import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.ashley.utils.ImmutableArray;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.engine.Families;
+import com.mygdx.game.engine.Mappers;
 
 public class EnemyDropSystem extends EntitySystem {
 
@@ -19,6 +20,9 @@ public class EnemyDropSystem extends EntitySystem {
     public void update(float delta) {
         for (int i = 0; i < enemies.size(); i++) {
             Entity enemy = enemies.get(i);
+            if (Mappers.removable.get(enemy) != null) {
+                String deadEnemyName = Mappers.name.get(enemy).name;
+            }
         }
     }
 }
