5f48e4296d887b10b1bcfe23ee6e088103d60ce1
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-01 18:14:31 -0700

    make AffixesComponent

---
+++ core/src/com/mygdx/game/engine/components/inventory/items/individual/AffixesComponent.java
@@ -0,0 +1,9 @@
+package com.mygdx.game.engine.components.inventory.items.individual;
+
+import com.badlogic.ashley.core.Component;
+
+import java.util.HashMap;
+
+public class AffixesComponent implements Component {
+    HashMap<String, Float> affixes = new HashMap<>();
+}
