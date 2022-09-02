90d5ade99838a6d4ee03398697b6c1f6e43a9e02
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-02 15:50:09 -0700

    make ArmourBaseStatComponent

---
+++ core/src/com/mygdx/game/engine/components/inventory/items/individual/ArmourBaseStatComponent.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.engine.components.inventory.items.individual;
+
+import com.badlogic.ashley.core.Component;
+
+public class ArmourBaseStatComponent implements Component {
+    public int phyDef;
+    public int magDef;
+
+    public ArmourBaseStatComponent() {}
+
+    public ArmourBaseStatComponent(int phyDef, int magDef) {
+        this.phyDef = phyDef;
+        this.magDef = magDef;
+    }
+}
