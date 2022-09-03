9d055ff1c41e0a4344fde2f3cd145d213bf103d1
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-03 16:17:10 -0700

    make method to check if player over item drop

--- core/src/com/mygdx/game/engine/systems/ItemPickupSystem.java
+++ core/src/com/mygdx/game/engine/systems/ItemPickupSystem.java
@@ -3,9 +3,13 @@ package com.mygdx.game.engine.systems;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.math.Rectangle;
 import com.mygdx.game.GameMapProperties;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.engine.Families;
+import com.mygdx.game.engine.Mappers;
+import com.mygdx.game.engine.components.Position;
+import com.mygdx.game.engine.components.Size;
 import com.mygdx.game.utils.EntityTextureObject;
 
 public class ItemPickupSystem extends EntitySystem {
@@ -23,6 +27,15 @@ public class ItemPickupSystem extends EntitySystem {
     public void update(float delta) {
         for (int i = 0; i < enemyDrops.getCount(); i++) {
             EntityTextureObject textureObject = (EntityTextureObject) enemyDrops.get(i);
+            if (overItemDrop(textureObject)) System.out.println("pickup");
         }
+    }
+
+    private boolean overItemDrop(EntityTextureObject itemDrop) {
+        Position playerPos = Mappers.position.get(player);
+        Size playerSize = Mappers.size.get(player);
+        Rectangle playerRect = new Rectangle(playerPos.x, playerPos.y, playerSize.width, playerSize.height);
+        Rectangle itemDropRect = new Rectangle(itemDrop.getX(), itemDrop.getY(), itemDrop.getTextureRegion().getRegionWidth(), itemDrop.getTextureRegion().getRegionHeight());
+        return playerRect.overlaps(itemDropRect);
     }
 }
