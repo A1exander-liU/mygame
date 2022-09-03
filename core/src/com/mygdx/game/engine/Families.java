9273bf19d63c2742af08537e4f32d287ed43ef04
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-02 17:20:56 -0700

    add itemDrops family

--- core/src/com/mygdx/game/engine/Families.java
+++ core/src/com/mygdx/game/engine/Families.java
@@ -3,6 +3,7 @@ package com.mygdx.game.engine;
 import com.badlogic.ashley.core.Family;
 import com.mygdx.game.engine.components.CollidableComponent;
 import com.mygdx.game.engine.components.Enemy;
+import com.mygdx.game.engine.components.Position;
 import com.mygdx.game.engine.components.inventory.items.shared.InventoryItemComponent;
 import com.mygdx.game.engine.components.ObstacleComponent;
 import com.mygdx.game.engine.components.Player;
@@ -17,6 +18,7 @@ public class Families {
     public static final Family collisions = Family.all(CollidableComponent.class).get();
     public static final Family characters = Family.one(Player.class, Enemy.class).get();
     public static final Family items = Family.all(InventoryItemComponent.class).get();
+    public static final Family itemDrops = Family.all(InventoryItemComponent.class, Position.class).get();
 
     public Families() {}
     
