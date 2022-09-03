1e208326969e0582d5d9bdb8c436548e17a576a0
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-03 16:55:01 -0700

    remove stackable item drop after adding it

--- core/src/com/mygdx/game/engine/systems/ItemPickupSystem.java
+++ core/src/com/mygdx/game/engine/systems/ItemPickupSystem.java
@@ -5,6 +5,7 @@ import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.math.Rectangle;
 import com.mygdx.game.GameMapProperties;
+import com.mygdx.game.InventoryUi;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.engine.Families;
 import com.mygdx.game.engine.Mappers;
@@ -16,18 +17,23 @@ public class ItemPickupSystem extends EntitySystem {
 
     Entity player;
     MapObjects enemyDrops;
+    InventoryUi inventoryUi;
 
     public ItemPickupSystem() {
         super(96);
         player = MyGame.engine.getEntitiesFor(Families.player).get(0);
         enemyDrops = MyGame.gameMapProperties.getMapLayer(GameMapProperties.ENEMY_DROPS).getObjects();
+        inventoryUi = new InventoryUi();
     }
 
     @Override
     public void update(float delta) {
         for (int i = 0; i < enemyDrops.getCount(); i++) {
             EntityTextureObject textureObject = (EntityTextureObject) enemyDrops.get(i);
-            if (overItemDrop(textureObject)) System.out.println("pickup");
+            if (overItemDrop(textureObject)) {
+                inventoryUi.addToInventory(textureObject.getOwner());
+                removeFromEnemyDropsLayer(textureObject);
+            }
         }
     }
 
@@ -37,5 +43,9 @@ public class ItemPickupSystem extends EntitySystem {
         Rectangle playerRect = new Rectangle(playerPos.x, playerPos.y, playerSize.width, playerSize.height);
         Rectangle itemDropRect = new Rectangle(itemDrop.getX(), itemDrop.getY(), itemDrop.getTextureRegion().getRegionWidth(), itemDrop.getTextureRegion().getRegionHeight());
         return playerRect.overlaps(itemDropRect);
+    }
+
+    private void removeFromEnemyDropsLayer(EntityTextureObject textureObject) {
+        MyGame.gameMapProperties.getMapLayer(GameMapProperties.ENEMY_DROPS).getObjects().remove(textureObject);
     }
 }
