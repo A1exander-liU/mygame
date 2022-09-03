a8a17b9e55381ec4a5b1dd72c5e0877db526ca38
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-03 16:11:24 -0700

    start on ItemPickupSystem

---
+++ core/src/com/mygdx/game/engine/systems/ItemPickupSystem.java
@@ -0,0 +1,27 @@
+package com.mygdx.game.engine.systems;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.EntitySystem;
+import com.badlogic.gdx.maps.MapObjects;
+import com.mygdx.game.GameMapProperties;
+import com.mygdx.game.MyGame;
+import com.mygdx.game.engine.Families;
+import com.mygdx.game.utils.EntityTextureObject;
+
+public class ItemPickupSystem extends EntitySystem {
+
+    Entity player;
+    MapObjects enemyDrops;
+
+    public ItemPickupSystem() {
+        player = MyGame.engine.getEntitiesFor(Families.player).get(0);
+        enemyDrops = MyGame.gameMapProperties.getMapLayer(GameMapProperties.ENEMY_DROPS).getObjects();
+    }
+
+    @Override
+    public void update(float delta) {
+        for (int i = 0; i < enemyDrops.getCount(); i++) {
+            EntityTextureObject textureObject = (EntityTextureObject) enemyDrops.get(i);
+        }
+    }
+}
