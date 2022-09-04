1b26ce7b6297b46901d2403f141faec8db2f8ef9
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-03 17:30:39 -0700

    remove centering of all the labels

--- core/src/com/mygdx/game/ItemInfoDialog.java
+++ core/src/com/mygdx/game/ItemInfoDialog.java
@@ -115,14 +115,12 @@ public class ItemInfoDialog extends Dialog {
     public void addWeaponInfo() {
         WeaponBaseStatComponent weaponBaseStat = Mappers.weaponBaseStat.get(item);
         AffixesComponent affixes = Mappers.affixes.get(item);
+
         Label dmgTitle = new Label("DMG", getSkin(), "pixel2D", Color.BLACK);
-        dmgTitle.setAlignment(Align.center);
         Label atkSpdTitle = new Label("Attack Spd", getSkin(), "pixel2D", Color.BLACK);
-        atkSpdTitle.setAlignment(Align.center);
         Label dmg = new Label(weaponBaseStat.minDmg + " - " + weaponBaseStat.maxDmg, getSkin(), "pixel2D", Color.BLACK);
-        dmg.setAlignment(Align.center);
         Label atkSpd = new Label("" + weaponBaseStat.attackDelay, getSkin(), "pixel2D", Color.BLACK);
-        atkSpd.setAlignment(Align.center);
+
         Label desc = new Label("\"" + Mappers.description.get(item).description + "\"", getSkin(), "pixel2D", Color.BLACK);
         desc.setAlignment(Align.center);
         desc.setWrap(true);
@@ -141,9 +139,7 @@ public class ItemInfoDialog extends Dialog {
             CharAttributes affix = affixes.affixes.get(i);
             // make label for name of the affix and the value
             Label affixName = new Label(affix.getAttributeName(), getSkin(), "pixel2D", Color.BLACK);
-            affixName.setAlignment(Align.center);
             Label affixValue = new Label(""+affix.getValue(), getSkin(), "pixel2D", Color.BLACK);
-            affixValue.setAlignment(Align.center);
             weaponStatTable.add(affixValue);
             weaponStatTable.add(affixName);
             weaponStatTable.row();
