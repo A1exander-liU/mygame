241f63a81726539021dd317d10b5ae11c2c72c4e
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-04 15:20:42 -0700

    start on save test

---
+++ core/src/com/mygdx/game/engine/systems/saving/SaveTest.java
@@ -0,0 +1,13 @@
+package com.mygdx.game.engine.systems.saving;
+
+import com.badlogic.ashley.core.EntitySystem;
+
+public class SaveTest extends EntitySystem {
+
+    public SaveTest() {}
+
+    @Override
+    public void update(float delta) {
+        
+    }
+}
