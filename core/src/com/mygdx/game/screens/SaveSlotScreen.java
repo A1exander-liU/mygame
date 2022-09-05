6f038b898a2e3a62a27a3c06f3abf26386b04fcf
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-05 16:46:26 -0700

    make stage and root table

--- core/src/com/mygdx/game/screens/SaveSlotScreen.java
+++ core/src/com/mygdx/game/screens/SaveSlotScreen.java
@@ -2,19 +2,45 @@ package com.mygdx.game.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.mygdx.game.MyGame;
 
 public class SaveSlotScreen implements Screen {
     MyGame parent;
+    Stage stage;
 
     public SaveSlotScreen(MyGame parent) {
         this.parent = parent;
+        stage = new Stage(new ScreenViewport());
     }
 
     @Override
     public void show() {
+        stage.clear();
+        Gdx.input.setInputProcessor(stage);
+        Table table = new Table();
+        table.setFillParent(true);
+        table.setDebug(false);
+        stage.addActor(table);
 
+        Skin gameUiSkin = new Skin();
+
+        FreeTypeFontGenerator generator  = new FreeTypeFontGenerator(Gdx.files.internal("fonts/PressStart2P-Regular.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 16;
+        parameter.color = Color.BLACK;
+        BitmapFont font = generator.generateFont(parameter);
+        gameUiSkin.add("pixel2D", font);
+        gameUiSkin.addRegions(new TextureAtlas("Game_UI_Skin/Game_UI_Skin.atlas"));
+        gameUiSkin.load(Gdx.files.internal("Game_UI_Skin/Game_UI_Skin.json"));
     }
 
     @Override
