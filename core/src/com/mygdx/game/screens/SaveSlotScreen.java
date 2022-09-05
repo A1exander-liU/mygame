2c1cec568f5082d049ec7a43d73b843131ef90da
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-05 16:38:43 -0700

    make background colour and clear each frame

--- core/src/com/mygdx/game/screens/SaveSlotScreen.java
+++ core/src/com/mygdx/game/screens/SaveSlotScreen.java
@@ -1,6 +1,8 @@
 package com.mygdx.game.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.mygdx.game.MyGame;
 
 public class SaveSlotScreen implements Screen {
@@ -17,7 +19,8 @@ public class SaveSlotScreen implements Screen {
 
     @Override
     public void render(float delta) {
-
+        Gdx.gl.glClearColor(0.8824f, 0.7765f, 0.6f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
     @Override
