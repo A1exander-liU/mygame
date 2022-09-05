d3027028d6df5dfb93d940f9e08b8c358dc832a3
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-05 16:37:19 -0700

    start on SaveSlotScreen

---
+++ core/src/com/mygdx/game/screens/SaveSlotScreen.java
@@ -0,0 +1,47 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.Screen;
+import com.mygdx.game.MyGame;
+
+public class SaveSlotScreen implements Screen {
+    MyGame parent;
+
+    public SaveSlotScreen(MyGame parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
