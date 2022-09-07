2b0fcedaa95b4dc4c03c0ed870ff4f5d5f69d4b2
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-07 15:36:26 -0700

    make and main menu and save buttons

--- core/src/com/mygdx/game/utils/ui/PauseDialog.java
+++ core/src/com/mygdx/game/utils/ui/PauseDialog.java
@@ -2,15 +2,24 @@ package com.mygdx.game.utils.ui;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class PauseDialog extends Dialog {
 
     public PauseDialog(String title, Skin skin) {
         super(title, skin);
+        buildPauseDialog();
     }
 
     @Override
     protected void result(Object result) {
         // hide to remove
+    }
+
+    private void buildPauseDialog() {
+        getButtonTable().defaults().expand().fill().pad(5);
+        button("Main Menu", "mainMenu");
+        getButtonTable().row();
+        button("Save", "save");
     }
 }
