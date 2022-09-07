78dab545c6bf3a6e2a6f2afce850424dfae03da7
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-07 15:57:40 -0700

    center title label

--- core/src/com/mygdx/game/utils/ui/PauseDialog.java
+++ core/src/com/mygdx/game/utils/ui/PauseDialog.java
@@ -1,9 +1,9 @@
 package com.mygdx.game.utils.ui;
 
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.Align;
 
 public class PauseDialog extends Dialog {
 
@@ -19,6 +19,7 @@ public class PauseDialog extends Dialog {
 
     private void buildPauseDialog() {
         getTitleLabel().setText("Paused");
+        getTitleLabel().setAlignment(Align.center);
         getTitleTable().add(new TextButton("x", getSkin())).expand().right().pad(10,10,0,0);
         getButtonTable().defaults().expand().fill().pad(5);
         button("Main Menu", "mainMenu");
