567ce24c96a90041a706dc04dd2f699f1861a2d7
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-07 15:32:56 -0700

    initialize and show PauseDialog when click pause

--- core/src/com/mygdx/game/utils/ui/PauseButton.java
+++ core/src/com/mygdx/game/utils/ui/PauseButton.java
@@ -1,9 +1,9 @@
 package com.mygdx.game.utils.ui;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 public class PauseButton extends ImageButton {
@@ -11,12 +11,17 @@ public class PauseButton extends ImageButton {
     public PauseButton(Skin skin) {
         super(skin, "image-button-pause");
         addListener(new ChangeListener() {
-            @Override
             public void changed(ChangeEvent event, Actor actor) {
                 // open up a window to go back to main menu screen
                 // and put save button here too
-
+                System.out.println("clicked");
+                showPauseMenu();
             }
         });
+    }
+
+    private void showPauseMenu() {
+        PauseDialog pauseDialog = new PauseDialog("", getSkin());
+        pauseDialog.show(getStage());
     }
 }
