e29091a05b986cb5f5496639bb75f92728e94a52
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-10 19:59:37 -0700

    add MyGame and saveSlot field

--- core/src/com/mygdx/game/utils/ui/DeleteSaveButton.java
+++ core/src/com/mygdx/game/utils/ui/DeleteSaveButton.java
@@ -1,14 +1,27 @@
 package com.mygdx.game.utils.ui;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.mygdx.game.MyGame;
 
 public class DeleteSaveButton extends TextButton {
+    MyGame root;
+    String saveSlot;
 
-    public DeleteSaveButton(String text, Skin skin) {
+    public DeleteSaveButton(String text, Skin skin, MyGame root, String saveSlot) {
         super(text, skin);
+        this.root = root;
+        this.saveSlot = saveSlot;
         setLabel(new Label("x", skin, "pixel2D", Color.BLACK));
+        addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+
+            }
+        });
     }
 }
