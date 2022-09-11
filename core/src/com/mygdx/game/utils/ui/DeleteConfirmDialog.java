e033e61023d624e417dead26bf830630e6bec401
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-10 20:06:37 -0700

    check if result is yes

--- core/src/com/mygdx/game/utils/ui/DeleteConfirmDialog.java
+++ core/src/com/mygdx/game/utils/ui/DeleteConfirmDialog.java
@@ -17,6 +17,9 @@ public class DeleteConfirmDialog extends Dialog {
 
     @Override
     protected void result(Object result) {
+        if (result == "yes") {
+            
+        }
     }
 
     private void buildDialog() {
