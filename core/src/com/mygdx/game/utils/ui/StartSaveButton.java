e881f37f2b2091d0417646214f20958b24fe10dc
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-10 19:11:41 -0700

    set root and saveSlot fields

--- core/src/com/mygdx/game/utils/ui/StartSaveButton.java
+++ core/src/com/mygdx/game/utils/ui/StartSaveButton.java
@@ -11,25 +11,31 @@ import com.mygdx.game.MyGame;
 import java.util.Objects;
 
 public class StartSaveButton extends TextButton {
+    MyGame root;
+    String saveSlot;
     // will be the button to click to start the game
     // will either be continue or new depending if the slot has save data
-    public StartSaveButton(String text, Skin skin, MyGame root, String slot) {
+    public StartSaveButton(String text, Skin skin, MyGame root, String saveSlot) {
         super(text, skin);
-
+        this.root = root;
+        this.saveSlot = saveSlot;
+        // if slot is empty, change to New
         if (Objects.equals(root.getSaveStates().getSlotSerializedData(slot), "")) {
             setLabel(new Label("New", skin, "pixel2D", Color.BLACK));
         }
+        // if the slot has save data, change to Continue
         else {
             setLabel(new Label("Continue", skin, "pixel2D", Color.BLACK));
         }
-
-
-
         addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-
+                startSave();
             }
         });
+    }
+
+    private void startSave() {
+
     }
 }
