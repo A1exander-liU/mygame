e31ab777029005a8e2c7476e6c6346fd19bb5439
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-11 15:52:50 -0700

    start on NewPlayerScreen

---
+++ core/src/com/mygdx/game/screens/NewPlayerScreen.java
@@ -0,0 +1,51 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.mygdx.game.MyGame;
+
+public class NewPlayerScreen implements Screen {
+    MyGame parent;
+    Stage stage;
+
+    public NewPlayerScreen(MyGame parent) {
+        this.parent = parent;
+        this.stage = new Stage(new ScreenViewport());
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
