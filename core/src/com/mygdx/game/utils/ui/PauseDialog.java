282c30656f7498d3148b1cc5a08ea35e334281d1
AlexanderLiu <alexanderliu2016@gmail.com>
2022-09-11 15:30:22 -0700

    add functionality to save button in PauseDialog

--- core/src/com/mygdx/game/utils/ui/PauseDialog.java
+++ core/src/com/mygdx/game/utils/ui/PauseDialog.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.utils.ui;
 
+import com.badlogic.ashley.core.Family;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -7,6 +8,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.Align;
 import com.mygdx.game.MyGame;
+import com.mygdx.game.engine.utils.componentutils.Families;
+import com.mygdx.game.engine.utils.entities.PlayerEntity;
 
 public class PauseDialog extends Dialog {
     MyGame root;
@@ -23,6 +26,10 @@ public class PauseDialog extends Dialog {
         // hide to remove
         if (result == "mainMenu") {
             root.changeScreen(MyGame.MENU_SCREEN);
+        }
+        if (result == "save") {
+            System.out.println("saved");
+            root.saveData.save((PlayerEntity) MyGame.engine.getEntitiesFor(Families.player).get(0));
         }
     }
 
